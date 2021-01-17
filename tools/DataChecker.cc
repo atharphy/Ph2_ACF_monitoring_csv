de2c5f8f3b0d3601523321acfb41c70adbfeae95
Sarah Seif El Nasr <sarah.storey@cern.ch>
2021-01-17 14:37:40 +0000

    Update DataChecker.cc - fix typo when setting output pad 4 of MPA

--- tools/DataChecker.cc
+++ tools/DataChecker.cc
@@ -692,7 +692,7 @@ void DataChecker::DigitalInjectionTest(bool pBypassCic,bool pShiftRegMode)
                     fReadoutChipInterface->WriteChipReg(cChip,"Out1",4);  
                     fReadoutChipInterface->WriteChipReg(cChip,"Out2",3);  
                     fReadoutChipInterface->WriteChipReg(cChip,"Out3",2);  
-                    fReadoutChipInterface->WriteChipReg(cChip,"Out4",4);  
+                    fReadoutChipInterface->WriteChipReg(cChip,"Out4",1);  
                     fReadoutChipInterface->WriteChipReg(cChip,"Out5",0);//L1 line 
 
 
