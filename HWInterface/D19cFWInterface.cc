2699486bf0a82ea49526f35d9aef0e8720ddcb8b
Kevin Nash <knash201@gmail.com>
2021-01-22 04:01:49 -0600

    Cleaning up async counter read-back

--- HWInterface/D19cFWInterface.cc
+++ HWInterface/D19cFWInterface.cc
@@ -1989,6 +1989,9 @@ void D19cFWInterface::ReadPSCounters(BeBoard* pBoard, std::vector<uint32_t>& pDa
                                 cDataWord = (cDataWord) | (cCounterValue << (cWordCounter & 0x1) * 16);
                                 if((cWordCounter & 0x1) == 1)
                                 {
+                                    LOG (INFO) << BOLDBLUE << "Word#" << +cWordCounter 
+                                        << std::bitset<32>(cDataWord) 
+                                        << RESET;
                                     pData.push_back(cDataWord);
                                     cDataWord = 0x0000;
                                 }
