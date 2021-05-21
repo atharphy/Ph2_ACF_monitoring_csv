3777cfeaed220047af602d9abe40d4ae50dff1d6
Sarah Seif El Nasr-Storey <Sarah.Storey@cern.ch>
2021-05-21 11:40:39 +0200

    Fix to summary plot for module with missing chips - PedeNoise

--- DQMUtils/DQMHistogramPedeNoise.cc
+++ DQMUtils/DQMHistogramPedeNoise.cc
@@ -421,23 +421,23 @@ void DQMHistogramPedeNoise::fillPedestalAndNoisePlots(DetectorDataContainer& the
                         {
                             chipStripNoiseEvenHistogram->SetBinContent(int(channelNumber / 2) + 1, cNoise);
                             chipStripNoiseEvenHistogram->SetBinError(int(channelNumber / 2) + 1, cNoiseErr);
-                            hybridStripNoiseEvenHistogram->SetBinContent(NCHANNELS / 2 * chip->getIndex() + int(channelNumber / 2) + 1, cNoise);
-                            hybridStripNoiseEvenHistogram->SetBinError(NCHANNELS / 2 * chip->getIndex() + int(channelNumber / 2) + 1, cNoiseErr);
+                            hybridStripNoiseEvenHistogram->SetBinContent(NCHANNELS / 2 * chip->getId() + int(channelNumber / 2) + 1, cNoise);
+                            hybridStripNoiseEvenHistogram->SetBinError(NCHANNELS / 2 * chip->getId() + int(channelNumber / 2) + 1, cNoiseErr);
                         }
                         else
                         {
                             chipStripNoiseOddHistogram->SetBinContent(int(channelNumber / 2) + 1, cNoise);
                             chipStripNoiseOddHistogram->SetBinError(int(channelNumber / 2) + 1, cNoiseErr);
-                            hybridStripNoiseOddHistogram->SetBinContent(NCHANNELS / 2 * chip->getIndex() + int(channelNumber / 2) + 1, cNoise);
-                            hybridStripNoiseOddHistogram->SetBinError(NCHANNELS / 2 * chip->getIndex() + int(channelNumber / 2) + 1,cNoiseErr);
+                            hybridStripNoiseOddHistogram->SetBinContent(NCHANNELS / 2 * chip->getId() + int(channelNumber / 2) + 1, cNoise);
+                            hybridStripNoiseOddHistogram->SetBinError(NCHANNELS / 2 * chip->getId() + int(channelNumber / 2) + 1,cNoiseErr);
                         }
 
                         chipStripNoiseHistogram->SetBinContent(channelNumber + 1, cNoise);
                         chipStripNoiseHistogram->SetBinError(channelNumber + 1, cNoiseErr);
                         chipStripPedestalHistogram->SetBinContent(channelNumber + 1,  cPedestal);
                         chipStripPedestalHistogram->SetBinError(channelNumber + 1, cPedestalErr);
-                        hybridStripNoiseHistogram->SetBinContent(NCHANNELS * chip->getIndex() + channelNumber + 1, cNoise);
-                        hybridStripNoiseHistogram->SetBinError(NCHANNELS * chip->getIndex() + channelNumber + 1, cNoiseErr);
+                        hybridStripNoiseHistogram->SetBinContent(NCHANNELS * chip->getId() + channelNumber + 1, cNoise);
+                        hybridStripNoiseHistogram->SetBinError(NCHANNELS * chip->getId() + channelNumber + 1, cNoiseErr);
 
                         ++channelNumber;
                     }
