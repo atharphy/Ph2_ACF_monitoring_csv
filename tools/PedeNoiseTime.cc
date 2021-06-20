2534d6e8d794644aa77a5367f06bd8067c29eab0
xtaldaq <xtaldaq@ph2acfblade01.dyndns.cern.ch>
2021-06-20 18:00:24 +0200

    Back to constant

--- tools/PedeNoiseTime.cc
+++ tools/PedeNoiseTime.cc
@@ -459,7 +459,7 @@ void PedeNoiseTime::GenericTriggers(size_t pNtriggersToSend, int pTriggerSeparat
         }
         // how many clocks to wait until
         // next trigger
-        size_t cTriggerGap = pNtriggersToSend;//std::round(cDistTrigSep(cGen));
+        size_t cTriggerGap = pTriggerSeparation;//std::round(cDistTrigSep(cGen));
         for(size_t cBx = 0; cBx < cTriggerGap; cBx++)
         {
             if(fFastCommands.size() == cMaxDepth) continue;
