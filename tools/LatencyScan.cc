d1808c0738df72e5a075cddba55674ca4f77abdc
xtaldaq <xtaldaq@ph2acfblade01.dyndns.cern.ch>
2021-06-22 14:12:34 +0200

    Forcing scan range when stub latency already found in BE alignment

--- tools/LatencyScan.cc
+++ tools/LatencyScan.cc
@@ -218,8 +218,8 @@ void LatencyScan::StubLatencyScan()
                         if( cChip->getIndex() > 0 ) break;
 
                         auto cTriggerLatency = fReadoutChipInterface->ReadChipReg(cChip,"TriggerLatency"); 
-                        fStartLatency = cTriggerLatency - cStubOffset - 5;
-                        fLatencyRange = 10;  
+                        fLatencyRange = 4;  
+                        fStartLatency = cTriggerLatency - cStubOffset - fLatencyRange/2;
                         LOG (INFO) << BOLDMAGENTA << "Using latency value programmed in Chp#"
                             << +cChip->getId() 
                             << " : modifying range of scan .. to start  at "
