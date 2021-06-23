b12e54232a89cd4b307b989c2666b1571e6d3359
xtaldaq <xtaldaq@ph2acfblade01.dyndns.cern.ch>
2021-06-23 12:53:04 +0200

    Clean-up .. DEBUG

--- tools/LatencyScan.cc
+++ tools/LatencyScan.cc
@@ -263,14 +263,14 @@ void LatencyScan::StubLatencyScan()
                                 auto cHits = cEvent->GetHits(cHybrid->getId(), cChip->getId());
                                 if(cHits.size() == 0) continue;
 
-                                for(auto cHit : cHits )
-                                {
-                                    if(cEventCount % cDebugOut == 0) LOG(INFO) << BOLDYELLOW << "\t\t\t...Hybrid#" << +cHybrid->getId() 
-                                            << " chip#" << +cChip->getId() 
-                                            << " hit found in channel " 
-                                            << +cHit
-                                            << RESET;
-                                }
+                                // for(auto cHit : cHits )
+                                // {
+                                //     if(cEventCount % cDebugOut == 0) LOG(INFO) << BOLDYELLOW << "\t\t\t...Hybrid#" << +cHybrid->getId() 
+                                //             << " chip#" << +cChip->getId() 
+                                //             << " hit found in channel " 
+                                //             << +cHit
+                                //             << RESET;
+                                // }
                                 auto                 cReadoutChipInterface = static_cast<CbcInterface*>(fReadoutChipInterface);
                                 std::vector<uint8_t> cBendLUT              = cReadoutChipInterface->readLUT(cChip);
                                 auto                 cStubs                = cEvent->StubVector(cHybrid->getId(), cChip->getId());
@@ -285,10 +285,10 @@ void LatencyScan::StubLatencyScan()
                                     uint16_t cIndex        = std::distance(cBendLUT.begin(), cIter);
                                     int      cBend         = (0.5 * cIndex + (-7.0)) * 2.0;
                                     auto     cExpectedHits = cReadoutChipInterface->stubInjectionPattern(cChip, cStub.getPosition(), cBend);
-                                    if(cEventCount % cDebugOut == 0) LOG(INFO) << BOLDYELLOW << "\t\t\t...Hybrid#" << +cHybrid->getId() 
-                                            << " chip#" << +cChip->getId() 
-                                            << " stub with seed " << +cStub.getPosition() << " and bendCode " << +cStub.getBend() << " which is bend " << +cBend << " half-strips"
-                                            << RESET;
+                                    // if(cEventCount % cDebugOut == 0) LOG(INFO) << BOLDYELLOW << "\t\t\t...Hybrid#" << +cHybrid->getId() 
+                                    //         << " chip#" << +cChip->getId() 
+                                    //         << " stub with seed " << +cStub.getPosition() << " and bendCode " << +cStub.getBend() << " which is bend " << +cBend << " half-strips"
+                                    //         << RESET;
                                     // check that the hits from these stubs
                                     // match the hits in the event
                                     for(auto cHit: cExpectedHits)
