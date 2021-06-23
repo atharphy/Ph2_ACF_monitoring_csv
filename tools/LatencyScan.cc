642c1efc5dbf58ea5dcdb44142b6072be716cfb9
xtaldaq <xtaldaq@ph2acfblade01.dyndns.cern.ch>
2021-06-23 11:04:33 +0200

    DEBUG - fix to term for 2S-FEHs

--- tools/LatencyScan.cc
+++ tools/LatencyScan.cc
@@ -276,8 +276,10 @@ void LatencyScan::StubLatencyScan()
                                     uint16_t cIndex        = std::distance(cBendLUT.begin(), cIter);
                                     int      cBend         = (0.5 * cIndex + (-7.0)) * 2.0;
                                     auto     cExpectedHits = cReadoutChipInterface->stubInjectionPattern(cChip, cStub.getPosition(), cBend);
-                                    if(cEventCount % cDebugOut == 0) LOG(INFO) << BOLDBLUE << "\t\t\t...Hybrid#" << +cHybrid->getId() << " stub with seed " << +cStub.getPosition() << " and bendCode " << +cStub.getBend() << " which is bend " << +cBend << " half-strips"
-                                              << RESET;
+                                    if(cEventCount % cDebugOut == 0) LOG(INFO) << BOLDBLUE << "\t\t\t...Hybrid#" << +cHybrid->getId() 
+                                            << " chip#" << +cChip->getId() 
+                                            << " stub with seed " << +cStub.getPosition() << " and bendCode " << +cStub.getBend() << " which is bend " << +cBend << " half-strips"
+                                            << RESET;
                                     // check that the hits from these stubs
                                     // match the hits in the event
                                     for(auto cHit: cExpectedHits)
