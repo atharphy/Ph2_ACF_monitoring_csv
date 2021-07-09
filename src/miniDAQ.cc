a210ee321a694e1f03963ad34334a6fdebb413ff
DAQ user <xtaldaq@ph2acf-01.dyndns.cern.ch>
2021-07-09 16:28:09 +0200

    Adjust SSA latency in miniDAQ

--- src/miniDAQ.cc
+++ src/miniDAQ.cc
@@ -211,7 +211,7 @@ int main(int argc, char* argv[])
                     {
                         cTool.fReadoutChipInterface->WriteChipReg(chip, "ENFLAGS_ALL", 0x1);
                         cTool.fReadoutChipInterface->WriteChipReg(chip, "Threshold", cPSmoduleSSAth);
-                        cTool.fReadoutChipInterface->WriteChipReg(chip, "TriggerLatency", cPSmoduleLat);
+                        cTool.fReadoutChipInterface->WriteChipReg(chip, "TriggerLatency", cPSmoduleLat-1);
                     }
                     if(chip->getFrontEndType() == FrontEndType::MPA)
                     {
