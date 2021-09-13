dc68ed841612042286cc552aefb4165345b761b3
Sarah <Sarah.Storey@cern.ch>
2021-09-13 16:30:02 +0200

    DEBUG - trying with just the page toggle

--- HWInterface/CbcInterface.cc
+++ HWInterface/CbcInterface.cc
@@ -560,8 +560,8 @@ bool CbcInterface::ConfigurePage(Chip* pCbc, uint8_t pPage, bool pVerifLoop)
     bool     cMapWasEmpty = false;
     if(cIter == fPageMap.end())
     {
-
         uint8_t cDefaultPage = ReadChipReg(pCbc,"Page"); 
+        LOG (INFO) << BOLDMAGENTA << "Default page on CBC" << +pCbc->getId() << " on hybrid " << +cCbc->getHybridId() << " is " << +cDefaultPage << RESET;
         fPageMap.insert(std::make_pair(cAddress, cDefaultPage));
         cMapWasEmpty=true;
     }
