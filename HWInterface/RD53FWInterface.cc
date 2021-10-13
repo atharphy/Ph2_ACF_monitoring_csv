fa1f629ec14f4d37adfa4fd1ea0e70def32f4996
Mauro <mauro.dinardo@cern.ch>
2021-10-13 16:25:59 +0200

    New change

--- HWInterface/RD53FWInterface.cc
+++ HWInterface/RD53FWInterface.cc
@@ -662,13 +662,13 @@ void RD53FWInterface::ReadNEvents(BeBoard* pBoard, uint32_t pNEvents, std::vecto
     // @TMP@
     if(RD53FWInterface::localCfgFastCmd.autozero_source == AutozeroSource::FastCMDFSM)
         RD53FWInterface::WriteChipCommand(RD53Cmd::WrReg(RD53Constants::BROADCAST_CHIPID, RD53Constants::GLOBAL_PULSE_ADDR, 1 << 14).getFrames(), -1);
-    else if(RD53FWInterface::localCfgFastCmd.autozero_source == AutozeroSource::UserDefined)
+    else if((RD53FWInterface::localCfgFastCmd.autozero_source == AutozeroSource::UserDefined) || (RD53FWInterface::localCfgFastCmd.autozero_source == AutozeroSource::Disabled))
     {
         RD53FWInterface::WriteChipCommand(RD53Cmd::WrReg(RD53Constants::BROADCAST_CHIPID, RD53Constants::GLOBAL_PULSE_ADDR, 1 << 14).getFrames(), -1);
         RD53FWInterface::WriteChipCommand(RD53Cmd::GlobalPulse(RD53Constants::BROADCAST_CHIPID, 0x6).getFrames(), -1);
-        std::this_thread::sleep_for(std::chrono::microseconds(2));
+        std::this_thread::sleep_for(std::chrono::microseconds(20));
         RD53FWInterface::WriteChipCommand(RD53Cmd::ECR().getFrames(), -1);
-        std::this_thread::sleep_for(std::chrono::microseconds(2));
+        std::this_thread::sleep_for(std::chrono::microseconds(50));
     }
 
     do
@@ -884,7 +884,8 @@ void RD53FWInterface::SetAndConfigureFastCommands(const BeBoard* pBoard,
     if(enableAutozero == true)
     {
         if(RD53FWInterface::localCfgFastCmd.trigger_source != TriggerSource::FastCMDFSM)
-            RD53FWInterface::localCfgFastCmd.autozero_source = AutozeroSource::UserDefined;
+            RD53FWInterface::localCfgFastCmd.autozero_source = AutozeroSource::Disabled;
+            // RD53FWInterface::localCfgFastCmd.autozero_source = AutozeroSource::UserDefined;
         else
             RD53FWInterface::localCfgFastCmd.autozero_source = AutozeroSource::FastCMDFSM;
         RD53FWInterface::localCfgFastCmd.fast_cmd_fsm.ecr_en               = true;
