695c9e6e6109b7fde8f3dd71675b843ac366532c
mod test <Sarah.Storey@cern.ch>
2021-10-22 09:59:41 -0500

    DDNU - SSA drive strength now loaded from settings file

--- System/SystemController.cc
+++ System/SystemController.cc
@@ -689,15 +689,15 @@ void SystemController::ModuleStartUpPS(const OpticalGroup* pOpticalGroup)
 
             bool     cSkipSSA3            = true; // eventually this needs to be set in the xml somewhere
             uint16_t cRegisterPadStrength = 0x1018;
-            uint8_t  cSLVSdriveSSA        = 7;
             for(uint8_t cSSAId = 0; cSSAId < 8; cSSAId++)
             {
                 if(cSkipSSA3 && cSSAId == 3) continue;
-
-                LOG(INFO) << BOLDMAGENTA << "SSA " << +cSSAId << " current set to " << +cSLVSdriveSSA << "" << RESET;
-                SSA* cSSA = new SSA(cHybrid->getBeBoardId(), cHybrid->getFMCId(), cHybrid->getId(), cSSAId, 0, 0, "./settings/SSAFiles/SSAPreCalibSYNC.txt");
+                
+                SSA* cSSA = new SSA(cHybrid->getBeBoardId(), cHybrid->getFMCId(), cHybrid->getId(), cSSAId, 0, 0, "./settings/SSAFiles/SSA.txt");
+                uint8_t cSLVSdriveSSA=cSSA->getReg("SLVS_pad_current");
                 cSSA->setOpticalId(cHybrid->getOpticalId());
                 cSSA->setOptical(cHybrid->isOptical());
+                LOG(INFO) << BOLDMAGENTA << "SSA " << +cSSAId << " current set to " << +cSLVSdriveSSA << "" << RESET;
                 (fBeBoardInterface->getFirmwareInterface())->WriteFERegister(cSSA, cRegisterPadStrength, cSLVSdriveSSA);
             }
 
