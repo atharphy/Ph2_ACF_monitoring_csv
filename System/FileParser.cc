e1e743677e4cb7bde37431fec18d1b70ab1f3ae6
Sarah <Sarah.Storey@cern.ch>
2021-12-22 17:29:04 +0100

    Move addresses to HWDescription; make sure mean across all chips is used for PedeEqu

--- System/FileParser.cc
+++ System/FileParser.cc
@@ -497,7 +497,6 @@ void FileParser::parseSSAContainer(pugi::xml_node pSSAnode, Hybrid* pHybrid, std
     cSSA->setNumberOfChannels(NSSACHANNELS);
     cSSA->setClockFrequency(320);
     cSSA->setMasterId(pHybrid->getMasterId());
-    cSSA->setChipAddress(0x20 + cChipId % 8);
 
     os << BOLDCYAN << "|"
        << "  "
@@ -641,7 +640,6 @@ void FileParser::parseSSA2Container(pugi::xml_node pSSAnode, Hybrid* pHybrid, st
     cSSA2->setOpticalId(pHybrid->getOpticalId());
     cSSA2->setNumberOfChannels(NSSACHANNELS);
     cSSA2->setClockFrequency(320);
-    cSSA2->setChipAddress(0x20 + cChipId % 8);
 }
 
 void FileParser::parseSSA2Settings(pugi::xml_node pHybridNode, ReadoutChip* pSSA)
@@ -668,7 +666,7 @@ void FileParser::parseMPAContainer(pugi::xml_node pMPANode, Hybrid* pHybrid, std
     cMPA->setNumberOfChannels(NSSACHANNELS, NMPACOLS);
     cMPA->setClockFrequency(320);
     cMPA->setMasterId(pHybrid->getMasterId());
-    cMPA->setChipAddress(0x40 + cChipId % 8);
+
 
     os << BOLDCYAN << "|"
        << "  "
@@ -920,7 +918,6 @@ void FileParser::parseHybridContainer(pugi::xml_node pHybridNode, OpticalGroup* 
                         cCic->setOptical(cHybrid->isOptical());
                         cCic->setOpticalId(cHybrid->getOpticalId());
                         cCic->setMasterId(cHybrid->getMasterId());
-                        cCic->setChipAddress(0x60);
 
                         os << GREEN << "|\t|\t|\t|----FrontEndType: ";
                         if(cType == FrontEndType::CIC)
@@ -1232,8 +1229,7 @@ void FileParser::parseCbcContainer(pugi::xml_node pCbcNode, Hybrid* cHybrid, std
     cCbc->setClockFrequency(320);
     cCbc->setNumberOfChannels(254);
     cCbc->setMasterId(cHybrid->getMasterId());
-    cCbc->setChipAddress(0x41 + cChipId % 8);
-
+    
     os << BOLDCYAN << "|"
        << "  "
        << "|"
