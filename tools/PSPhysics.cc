991e5816ce962e945339467e6fe41330a05db5c5
Sarah Seif El Nasr-Storey <Sarah.Storey@cern.ch>
2021-12-21 18:15:20 -0600

    Solved bugs

--- tools/PSPhysics.cc
+++ tools/PSPhysics.cc
@@ -85,7 +85,7 @@ void PSPhysics::ConfigureCalibration()
     // #######################
     // # Retrieve parameters #
     // #######################
-    saveRawData = this->findValueInSettings<bool>("SaveRawData");
+    saveRawData = this->findValueInSettings<double>("SaveRawData");
     doLocal     = false;
 
     // ###########################################
