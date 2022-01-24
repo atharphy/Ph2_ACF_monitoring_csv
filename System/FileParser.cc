0d2c450b23048cab0d43ab89b7619d9a1f0679ab
Sarah Seif El Nasr-Storey <Sarah.Storey@cern.ch>
2022-01-24 17:24:58 -0600

    Updated D19CDescription_2Sskeleton.xml and put back LpGBT OT configurations in FileParser

--- System/FileParser.cc
+++ System/FileParser.cc
@@ -262,6 +262,12 @@ void FileParser::parseOpticalGroupContainer(pugi::xml_node pOpticalGroupNode, Be
             os << BOLDBLUE << "|\t|----" << theChild.name() << " --> File: " << BOLDYELLOW << fileName << RESET << std::endl;
             lpGBT* thelpGBT = new lpGBT(cBoardId, cFMCId, cOpticalGroupId, fileName);
             thelpGBT->setOptical(pBoard->isOptical());
+
+            thelpGBT->setOpticalId(cOpticalGroupId);
+            thelpGBT->setChipAddress(0x70);               // default lpGBT address
+            thelpGBT->addRxGroups({0, 1, 2, 3, 4, 5, 6}); // be default we always use all 6 groups
+            thelpGBT->addRxChannels({0, 2});              // and always channel 0 and channel 2 of each group
+            
             theOpticalGroup->addlpGBT(thelpGBT);
 
             // ####################################################
