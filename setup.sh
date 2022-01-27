a51cd7e8c4436abfaf3a0d107a3a7176ecdd1f75
Younes Otarid <yotarid@cern.ch>
2022-01-27 18:59:25 +0100

    setting USB flags to false

--- setup.sh
+++ setup.sh
@@ -120,7 +120,7 @@ export CompileForShep=false
 export CompileWithEUDAQ=false
 
 # Compile with TC_USB library
-export CompileWithTCUSB=true
+export CompileWithTCUSB=false
 export UseTCUSBforROH=false
 export UseTCUSBTcpServer=false
 
