f3b000bc0b26ff03d0a2cc7c722b008b8e4cc064
Sarah <Sarah.Storey@cern.ch>
2022-02-16 17:31:32 +0100

    Adding cntrl reg to MPA2 + printout for SSA

--- HWDescription/SSA.cc
+++ HWDescription/SSA.cc
@@ -34,6 +34,7 @@ SSA::SSA(const FrontEndDescription& pFeDesc, uint8_t pSSAId, uint8_t pPartnerId,
     for(auto& cMapItem: fRegMap)
     {
         if(cMapItem.first.find("_ALL") == std::string::npos) continue;
+        LOG (INFO) << BOLDYELLOW << "Control register on SSA : " << cMapItem.first << RESET;
         cMapItem.second.fControlReg = 1;
     }
 }
@@ -50,6 +51,7 @@ SSA::SSA(uint8_t pBeId, uint8_t pFMCId, uint8_t pFeId, uint8_t pSSAId, uint8_t p
     for(auto& cMapItem: fRegMap)
     {
         if(cMapItem.first.find("_ALL") == std::string::npos) continue;
+        LOG (INFO) << BOLDYELLOW << "Control register on SSA : " << cMapItem.first << RESET;
         cMapItem.second.fControlReg = 1;
     }
 }
