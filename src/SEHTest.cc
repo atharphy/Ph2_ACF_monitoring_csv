3e19b3d1250cf782a5dcd3bb68433eddf03b303e
Alexander Pauls <alexander.pauls@rwth-aachen.de>
2022-05-04 15:44:19 +0200

    minor

--- src/SEHTest.cc
+++ src/SEHTest.cc
@@ -353,7 +353,7 @@ int main(int argc, char* argv[])
             else
             {
                 LOG(INFO) << BOLDRED << "CIC_Out test failed." << RESET;
-                //throw std::runtime_error(std::string("CIC_Out test failed."));
+                // throw std::runtime_error(std::string("CIC_Out test failed."));
             }
         }
     }
@@ -561,7 +561,7 @@ int main(int argc, char* argv[])
         cDebugInterface->L1ADebug();
     */
     // Save Result File
-    //cSEHTester.TurnOff();
+    // cSEHTester.TurnOff();
 
     if(cmd.foundOption("ext-leak") & cmd.foundOption("parallelHV"))
     {
