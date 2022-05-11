f26d9c407011bce199d1b6ce4324b191490b0963
Mauro <mauro.dinardo@cern.ch>
2022-05-11 18:39:01 +0200

    Minor

--- System/FileParser.cc
+++ System/FileParser.cc
@@ -276,7 +276,9 @@ void FileParser::parseOpticalGroupContainer(pugi::xml_node pOpticalGroupNode, Be
                 for(const pugi::xml_attribute& attr: theChild.attributes())
                 {
                     os << BOLDBLUE << "|\t|\t|---- " << attr.name() << ": " << BOLDYELLOW << attr.value() << "\n" << RESET;
-                    if(std::string(attr.name()) == "RxHSLPolarity")
+                    if(std::string(attr.name()) == "ChipAddress")
+                        thelpGBT->setChipAddress(convertAnyInt(theChild.attribute("ChipAddress").value()));
+                    else if(std::string(attr.name()) == "RxHSLPolarity")
                         thelpGBT->setRxHSLPolarity(theChild.attribute("RxHSLPolarity").as_int());
                     else if(std::string(attr.name()) == "TxHSLPolarity")
                         thelpGBT->setTxHSLPolarity(theChild.attribute("TxHSLPolarity").as_int());
