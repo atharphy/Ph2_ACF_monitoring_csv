3bc987f56e96632ff3afeb518c85bac2d507242e
Younes Otarid <yotarid@cern.ch>
2022-05-17 08:58:49 +0200

    reverting change to force enabling of data handshake in ReadEvent function - needed when specific number of events is requested

--- HWInterface/D19cL1ReadoutInterface.cc
+++ HWInterface/D19cL1ReadoutInterface.cc
@@ -239,9 +239,10 @@ bool D19cL1ReadoutInterface::ReadEvents(const BeBoard* pBoard)
         // clear internal data vector
         fData.clear();
         // configure readout
+        fHandshake                  = 1;
         auto cOriginalHandshakeMode = ReadReg("fc7_daq_cnfg.readout_block.global.data_handshake_enable");
         auto cOriginalPackNbr       = ReadReg("fc7_daq_cnfg.readout_block.packet_nbr");
-        WriteReg("fc7_daq_cnfg.readout_block.global.data_handshake_enable", cOriginalHandshakeMode);
+        WriteReg("fc7_daq_cnfg.readout_block.global.data_handshake_enable", fHandshake);
         auto cHandshake = ReadReg("fc7_daq_cnfg.readout_block.global.data_handshake_enable");
         // write number of triggers to accept
         // in the handshake mode offset is cleared after each handshake
