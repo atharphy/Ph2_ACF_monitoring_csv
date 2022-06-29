a36770704ade1c53966a2e0be4ef403887b7aad9
Mauro <mauro.dinardo@cern.ch>
2022-06-29 13:59:51 +0200

    Indentation

--- Utils/Bits/BitVector.h
+++ Utils/Bits/BitVector.h
@@ -1,29 +1,40 @@
-#ifndef BITS__BIT_VECTOR_HPP
-#define BITS__BIT_VECTOR_HPP
+/*!
+  \file                  BitVector.h
+  \brief                 BitVector implementation
+  \author                Alkiviadis PAPADOPOULOS
+  \version               1.0
+  \date                  28/06/22
+  Support:               email to mauro.dinardo@cern.ch
+  Support:               email to alkiviadis.papadopoulos@cern.ch
+*/
 
-#include "BitView.hpp"
+#ifndef BITS_BIT_VECTOR_HPP
+#define BITS_BIT_VECTOR_HPP
 
+#include "BitView.h"
 
 template <class BlockType>
-class BitVector {
+class BitVector
+{
     static constexpr size_t block_size = 8 * sizeof(BlockType);
-public:
 
+  public:
     using block_iterator = typename std::vector<BlockType>::iterator;
 
     BitVector() : _data(), _size(0) {}
 
     template <class C>
-    BitVector(C&& c, size_t size)
-     : _data(std::forward<C>(c))
-     , _size(size)
-    {}
+    BitVector(C&& c, size_t size) : _data(std::forward<C>(c)), _size(size)
+    {
+    }
 
     size_t size() const { return _size; }
 
-    BitView<BlockType> append_zeros(size_t n) {
+    BitView<BlockType> append_zeros(size_t n)
+    {
         int extra_bits = n + _size - _data.size() * block_size;
-        if (extra_bits > 0) {
+        if(extra_bits > 0)
+        {
             int extra_words = (extra_bits + block_size - 1) / block_size;
             _data.insert(std::end(_data), extra_words, 0);
         }
@@ -33,75 +44,75 @@ public:
     }
 
     template <class BlockTypeOther>
-    void append(const BitView<BlockTypeOther>& bits) {
+    void append(const BitView<BlockTypeOther>& bits)
+    {
         int extra_bits = bits.size() + _size - _data.size() * block_size;
-        
-        if (extra_bits > 0) {
+
+        if(extra_bits > 0)
+        {
             int extra_words = (extra_bits + block_size - 1) / block_size;
             _data.insert(std::end(_data), extra_words, 0);
         }
-        auto new_bits = BitView<BlockType>{_data.data(), _size, _size + bits.size()};
-        size_t offset = 0;
-        while (offset + 8 < bits.size()) {
+        auto   new_bits = BitView<BlockType>{_data.data(), _size, _size + bits.size()};
+        size_t offset   = 0;
+        while(offset + 8 < bits.size())
+        {
             uint8_t byte = bits.slice(offset, offset + 8).template get<uint8_t>();
             new_bits.slice(offset, offset + 8).set(byte);
             offset += 8;
         }
         int leftover_bits = bits.size() - offset;
-        if (leftover_bits > 0) {
+        if(leftover_bits > 0)
+        {
             uint8_t byte = bits.slice(offset, offset + leftover_bits).template get<uint8_t>();
             new_bits.slice(offset, offset + leftover_bits).set(byte);
         }
         _size += bits.size();
     }
 
-    block_iterator erase_blocks(block_iterator it) {
+    block_iterator erase_blocks(block_iterator it)
+    {
         _size = std::max(_size - block_size, 0ul);
         return _data.erase(it);
     }
 
-    block_iterator erase_blocks(block_iterator first, block_iterator last) {
+    block_iterator erase_blocks(block_iterator first, block_iterator last)
+    {
         _size = std::max(_size - block_size * (last - first), 0ul);
         return _data.erase(first, last);
     }
 
-    void clear() {
+    void clear()
+    {
         _data.clear();
         _size = 0;
     }
 
-    operator BitView<BlockType>() {
-        return {_data.data(), 0, _size};
-    }
+    operator BitView<BlockType>() { return {_data.data(), 0, _size}; }
 
-    operator BitView<const BlockType>() const {
-        return {_data.data(), 0, _size};
-    }
+    operator BitView<const BlockType>() const { return {_data.data(), 0, _size}; }
 
-    BitView<BlockType> view() {
-        return {_data.data(), 0, _size};
-    }
+    BitView<BlockType> view() { return {_data.data(), 0, _size}; }
 
-    
-    BitView<const BlockType> view() const {
-        return {_data.data(), 0, _size};
-    }
+    BitView<const BlockType> view() const { return {_data.data(), 0, _size}; }
 
-    std::vector<BlockType>& blocks() { return _data; }
+    std::vector<BlockType>&       blocks() { return _data; }
     const std::vector<BlockType>& blocks() const { return _data; }
 
-private:
+  private:
     std::vector<BlockType> _data;
-    size_t _size;
+    size_t                 _size;
 };
 
 template <class T>
-BitView<T> bit_view(BitVector<T>& vec) {
+BitView<T> bit_view(BitVector<T>& vec)
+{
     return vec;
 }
 
 template <class T>
-BitView<const T> bit_view(const BitVector<T>& vec) {
+BitView<const T> bit_view(const BitVector<T>& vec)
+{
     return vec;
 }
 
