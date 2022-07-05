1b0f30dd207fd15d18bc1723518bc49a02ec1ecf
Alexander Pauls <alexander.pauls@rwth-aachen.de>
2022-07-05 15:24:42 +0200

    Added (local) files to test SEH batch 2

---
+++ StartTest_v4_20220519_lpgbtv1.sh
@@ -0,0 +1,71 @@
+
+#!/bin/bash
+while getopts s:h:o:t: flag
+do
+    case "${flag}" in
+        s) slot=${OPTARG};;
+        h) hybridID=${OPTARG};;
+        o) operator=${OPTARG};;
+        t) testcardID=${OPTARG};;
+    esac
+done
+if [ $slot -lt 0 ] || [ $slot -gt 3 ]
+then
+    printf "Error: Slot = \"%s\" is not a valid choice. \nEnter -s [0,1,2,3]\n" $slot
+    exit -1
+fi
+if [ $hybridID -z ]
+then
+    printf "Error: Enter a hybrid ID\n"
+    exit -1
+fi
+
+cd /home/DAQUser/testcardDev/Ph2_ACF
+
+TIME=`date +"%Y-%m-%d_%H-%M-%S"`
+mkdir Results/SEH_${hybridID}_${TIME}
+mux_setup -f settings/mux_setup.xml --mux_configure 0,$slot | tee -a logs/Logfile_${hybridID}_${TIME}.log
+
+SEHTest -f settings/SEHTest.xml --powersupply --external-pattern 202 --fcmd-pattern 202 --scope-fcmd --clock-test  -a -r --eff --ext-leak 1000 --ext-bias 0x155 -v -i -b --hybridId ${hybridID}_noLoad --output Results/SEH_${hybridID}_${TIME}/ | tee -a logs/Logfile_noLoad_${hybridID}_${TIME}.log
+#SEHTest -f settings/SEHTest.xml --external-pattern 202 --fcmd-pattern 202 --scope-fcmd --clock-test  -a -r --eff -v -i -b --hybridId ${hybridID}_noLoad --output Results/SEH_${hybridID}_${TIME}/ | tee -a logs/Logfile_noLoad_${hybridID}_${TIME}.log
+
+if [$? -eq 255]
+then
+	mux_setup -f settings/mux_setup.xml --mux_configure 0,$slot | tee -a logs/Logfile_${hybridID}_${TIME}.log
+    #SEHTest -f settings/SEHTest.xml --external-pattern 202 --fcmd-pattern 170 --scope-fcmd --clock-test  -a -r --eff -v -i -b --hybridId ${hybridID}_noLoad --output Results/SEH_${hybridID}_${TIME}/ | tee -a logs/Logfile_noLoad_${hybridID}_${TIME}.log
+    SEHTest -f settings/SEHTest.xml --powersupply --external-pattern 202 --fcmd-pattern 170 --scope-fcmd --clock-test  -a -r --eff --ext-leak 1000 --ext-bias 0x155 -v -i -b --hybridId ${hybridID}_noLoad --output Results/SEH_${hybridID}_${TIME}/ | tee -a logs/Logfile_noLoad_${hybridID}_${TIME}.log
+fi
+
+mux_setup -f settings/mux_setup.xml --mux_configure 0,$slot | tee -a logs/Logfile_${hybridID}_${TIME}.log
+
+SEHTest -f settings/SEHTest.xml --external-pattern 202 --fcmd-pattern 170 --scope-fcmd --clock-test  -a -r -v -i -b --hybridId ${hybridID}_withLoad --output Results/SEH_${hybridID}_${TIME}/ --leftLoad 2000 --rightLoad 2000 | tee -a logs/Logfile_withLoad_${hybridID}_${TIME}.log
+
+if [$? -eq 255]
+then
+	mux_setup -f settings/mux_setup.xml --mux_configure 0,$slot | tee -a logs/Logfile_${hybridID}_${TIME}.log
+	SEHTest -f settings/SEHTest.xml --external-pattern 202 --fcmd-pattern 170 --scope-fcmd --clock-test  -a -r -v -i -b --hybridId ${hybridID}_withLoad --output Results/SEH_${hybridID}_${TIME}/ --leftLoad 2000 --rightLoad 2000 | tee -a logs/Logfile_withLoad_${hybridID}_${TIME}.log
+fi 
+mux_setup -f settings/mux_setup.xml --mux_disconnect 0,$slot | tee -a logs/Logfile_${hybridID}_${TIME}.log
+
+RESULTDIR=`ls Results -t | head -1`
+
+
+printf "[General]\n" | tee -a Results/${RESULTDIR}/RunInformation.txt
+printf "Slot = \"%s\"\n" $slot | tee -a Results/${RESULTDIR}/RunInformation.txt
+printf "HybridID = \"%s\"\n" $hybridID | tee -a Results/${RESULTDIR}/RunInformation.txt
+printf "Operator = \"%s\"\n" ${operator:="unknown"} | tee -a Results/${RESULTDIR}/RunInformation.txt
+printf "TestcardID = \"%s\"\n" ${testcardID:="unknown"} | tee -a Results/${RESULTDIR}/RunInformation.txt
+echo "Add a comment: "  
+read comment
+printf "Comment = \"%s\"\n" "$comment" | tee -a Results/${RESULTDIR}/RunInformation.txt
+printf "Temperature = \"20\"\n" | tee -a Results/${RESULTDIR}/RunInformation.txt
+for d in Results/$RESULTDIR/*/; do cp Results/$RESULTDIR/RunInformation.txt "$d"; done
+cp -r Results/${RESULTDIR} ../Data/
+#cp -r Results/${RESULTDIR} ../../Stecktest_Original/
+
+#cp -r Results/${RESULTDIR} ../../after_efusing/
+
+# -s : Test card slot - 3 is left, 0 is right
+# -h : Hybrid ID must be in format 201000027
+# -o : Operator (e.g., kklein, apauls ...)
+# -t : Testcard number 
