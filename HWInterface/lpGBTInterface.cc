f6843a14ee55a6f00f11a8ba36df75137256c323
Alexander Pauls <alexander.pauls@rwth-aachen.de>
2022-07-05 15:22:12 +0200

    Adding function to read fused values

--- HWInterface/lpGBTInterface.cc
+++ HWInterface/lpGBTInterface.cc
@@ -90,6 +90,31 @@ uint16_t lpGBTInterface::ReadChipReg(Chip* pChip, const std::string& pDacName)
     return cValue;
 }
 
+void lpGBTInterface::ReadChipFusedBlock(Ph2_HwDescription::Chip* pChip, uint8_t cFuseH, uint8_t cFuseL)
+{
+    WriteChipReg(pChip, "FUSEControl", 2);
+    int cReadBack = 0;
+    while(cReadBack != 4)
+    {
+        cReadBack = ReadChipReg(pChip, "FUSEStatus");
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        LOG(INFO) << BOLDGREEN << "lpgbt FUSEStatus = " << +cReadBack << RESET;
+    }
+    WriteChipReg(pChip, "FUSEBlowAddH", cFuseH);
+    WriteChipReg(pChip, "FUSEBlowAddL", cFuseL);
+    LOG(INFO) << BOLDGREEN << "lpgbt FUSEBlowAddH = " << +cFuseH << RESET;
+    LOG(INFO) << BOLDGREEN << "lpgbt FUSEBlowAddL = " << +cFuseL << RESET;
+    cReadBack = ReadChipReg(pChip, "FUSEValuesA");
+    LOG(INFO) << BOLDGREEN << "lpgbt FUSEValuesA = " << +cReadBack << RESET;
+    cReadBack = ReadChipReg(pChip, "FUSEValuesB");
+    LOG(INFO) << BOLDGREEN << "lpgbt FUSEValuesB = " << +cReadBack << RESET;
+    cReadBack = ReadChipReg(pChip, "FUSEValuesC");
+    LOG(INFO) << BOLDGREEN << "lpgbt FUSEValuesC = " << +cReadBack << RESET;
+    cReadBack = ReadChipReg(pChip, "FUSEValuesD");
+    LOG(INFO) << BOLDGREEN << "lpgbt FUSEValuesD = " << +cReadBack << RESET;
+    WriteChipReg(pChip, "FUSEControl", 0);
+}
+
 bool lpGBTInterface::WriteChipMultReg(Ph2_HwDescription::Chip* pChip, const std::vector<std::pair<std::string, uint16_t>>& pRegVec, bool pVerify)
 {
     bool writeGood = true;
