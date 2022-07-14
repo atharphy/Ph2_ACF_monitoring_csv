df5b2fd5d5c0b8b3428b9136cedeb12dfc93f071
Irene Mateos Domínguez <irene.mateos.dominguez@cern.ch>
2022-07-14 15:02:35 +0200

    Using chip->getSummary to get noise measurement data for TTree

--- tools/PedeNoise.cc
+++ tools/PedeNoise.cc
@@ -896,8 +896,7 @@ void PedeNoise::producePedeNoisePlots()
 #ifdef __USE_ROOT__
     fDQMHistogramPedeNoise.fillPedestalAndNoisePlots(*fThresholdAndNoiseContainer);
 
-    LOG(INFO) << "ProducingPedePlotsROOT" << RESET; // Can this stay...?
-    // Storing noise and pedestal average and RMS values on the summaryTree. Probably not the best way.
+    // Storing noise and pedestal average and RMS values on the summaryTree
     for(auto board: *fThresholdAndNoiseContainer)
     {
         for(auto opticalGroup: *board)
@@ -906,32 +905,14 @@ void PedeNoise::producePedeNoisePlots()
             {
                 for(auto chip: *module)
                 {
-                    delete gROOT->FindObject("auxPedestalDistribution");
-                    delete gROOT->FindObject("auxNoiseDistribution");
-
-                    TH1F* chipPedestalHistogram = new TH1F("auxPedestalDistribution", "Pedestal Distribution", 2048, -0.5, 1023.5);
-                    TH1F* chipNoiseHistogram    = new TH1F("auxNoiseDistribution", "Noise Distribution", 200, 0., 20.);
-
-                    for(uint8_t iChannel = 0; iChannel < chip->size(); ++iChannel)
-                    {
-                        // Check if NaN, if its NaN fill with whatever
-                        auto channelData = chip->getChannel<ThresholdAndNoise>(iChannel);
-                        chipPedestalHistogram->Fill(channelData.fThreshold);
-                        chipNoiseHistogram->Fill(channelData.fNoise);
-                    }
-                    LOG(DEBUG) << "Filling summary tree with noise measurements" << RESET;
-                    fillSummaryTree("AvgNoiseSSA" + std::to_string(chip->getId()), Double_t(chipNoiseHistogram->GetMean()));      // For GUI summaryTree
-                    fillSummaryTree("RMSNoiseSSA" + std::to_string(chip->getId()), Double_t(chipNoiseHistogram->GetRMS()));       // For GUI summaryTree
-                    fillSummaryTree("StDvNoiseSSA" + std::to_string(chip->getId()), Double_t(chipNoiseHistogram->GetStdDev()));   // For GUI summaryTree
-                    fillSummaryTree("AvgPedeSSA" + std::to_string(chip->getId()), Double_t(chipPedestalHistogram->GetMean()));    // For GUI summaryTree
-                    fillSummaryTree("RMSPedeSSA" + std::to_string(chip->getId()), Double_t(chipPedestalHistogram->GetRMS()));     // For GUI summaryTree
-                    fillSummaryTree("StDvPedeSSA" + std::to_string(chip->getId()), Double_t(chipPedestalHistogram->GetStdDev())); // For GUI summaryTree
+                    fillSummaryTree("AvgNoiseSSA" + std::to_string(chip->getId()), chip->getSummary<ThresholdAndNoise>().fNoise );      // For GUI summaryTree
+                    fillSummaryTree("StDvNoiseSSA" + std::to_string(chip->getId()), chip->getSummary<ThresholdAndNoise>().fNoiseError );   // For GUI summaryTree
+                    fillSummaryTree("AvgPedeSSA" + std::to_string(chip->getId()), chip->getSummary<ThresholdAndNoise>().fThreshold );    // For GUI summaryTree
+                    fillSummaryTree("StDvPedeSSA" + std::to_string(chip->getId()), chip->getSummary<ThresholdAndNoise>().fThresholdError ); // For GUI summaryTree
                 }
             }
         }
     }
-    delete gROOT->FindObject("auxPedestalDistribution");
-    delete gROOT->FindObject("auxNoiseDistribution");
 
 #else
     auto theThresholdAndNoiseStream = prepareChannelContainerStreamer<ThresholdAndNoise>();
