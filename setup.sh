8e5fcbc8ee7beafb5493d8632f433bf119e913e6
Mauro <mauro.dinardo@cern.ch>
2022-07-21 22:46:09 +0200

    Minor

--- setup.sh
+++ setup.sh
@@ -80,8 +80,8 @@ export EUDAQLIB=$EUDAQDIR/lib
 ############
 # Pybind11 #
 ############
-export PYBIND11=$PH2ACF_BASE_DIR/../pybind11/
-export PYBIND11INCLUDE=$PYBIND11/include
+#export PYBIND11=$PH2ACF_BASE_DIR/../pybind11-2.9.2/
+#export PYBIND11INCLUDE=$PYBIND11/include
 export PYTHONINCLUDE=/usr/include/python3.6m/
 
 ##########
