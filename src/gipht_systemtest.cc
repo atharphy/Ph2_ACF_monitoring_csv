83b1dac95462e03da314c8380e7df9ce0f14b57d
Stefan <stefan.maier@cern.ch>
2022-08-16 07:12:46 -0400

    updated gipht system test with correct xml

--- src/gipht_systemtest.cc
+++ src/gipht_systemtest.cc
@@ -25,7 +25,7 @@ int main(int argc, char** argv)
     // options
     cmd.setHelpOption("h", "help", "Print this help page");
 
-    cmd.defineOption("file", "Hw Description File . Default value: settings/D19CDescription.xml", ArgvParser::OptionRequiresValue /*| ArgvParser::OptionRequired*/);
+    cmd.defineOption("file", "Hw Description File . Default value: settings/2S_GIPHT.xml", ArgvParser::OptionRequiresValue /*| ArgvParser::OptionRequired*/);
     cmd.defineOptionAlternative("file", "f");
 
     cmd.defineOption("configure", "Configure HW", ArgvParser::NoOptionAttribute);
@@ -40,7 +40,7 @@ int main(int argc, char** argv)
     }
 
     // now query the parsing results
-    std::string cHWFile    = (cmd.foundOption("file")) ? cmd.optionValue("file") : "settings/gipht_2s_default.xml";
+    std::string cHWFile    = (cmd.foundOption("file")) ? cmd.optionValue("file") : "settings/2S_GIPHT.xml";
     bool        cConfigure = (cmd.foundOption("configure")) ? true : false;
 
     std::stringstream outp;
