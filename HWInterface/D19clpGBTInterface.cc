2da605313d88103ece14b190ebe71e2c50cad09e
Younes Otarid <yotarid@cern.ch>
2022-09-16 09:54:02 +0200

    [D19clpGBTInteface] (temp) Enabling lpGBT clocks on startup + but maybe I need to do it directly from the tool

--- HWInterface/D19clpGBTInterface.cc
+++ HWInterface/D19clpGBTInterface.cc
@@ -319,7 +319,7 @@ void D19clpGBTInterface::ConfigurePSROH(Ph2_HwDescription::Chip* pChip)
     // Clocks
     std::vector<uint8_t> cClocks = {fClock_LHS_Hybrid, fClock_LHS_CIC, fClock_RHS_Hybrid, fClock_RHS_CIC};
     // clock frequency set to 0 to disable it at first and only later configure what is needed
-    uint8_t cClkFreq = 0, cClkDriveStr = 1, cClkInvert = 1;
+    uint8_t cClkFreq = ((cChipRate == 5) ? 4 : 5), cClkDriveStr = 7, cClkInvert = 1;
     uint8_t cClkPreEmphWidth = 0, cClkPreEmphMode = 0, cClkPreEmphStr = 0;
     ConfigureClocks(pChip, cClocks, cClkFreq, cClkDriveStr, cClkInvert, cClkPreEmphWidth, cClkPreEmphMode, cClkPreEmphStr);
     // Tx Groups and Channels
