d6d8ebfec37f1ad2db0cc30c58fe4bfebce07fa2
Alexander Pauls <alexander.pauls@rwth-aachen.de>
2022-09-19 11:35:25 +0200

    minor

--- tools/SEHTester.cc
+++ tools/SEHTester.cc
@@ -550,14 +550,14 @@ void SEHTester::ExternalTestBiasVoltage(std::string powerSupplyId, std::string c
     TF1* fit_grading_HV_test = new TF1("fit_grading_HV_test", "pol1", 0, 1000);
     fit_grading_HV_test->SetParameter(0, 0);
     fit_grading_HV_test->SetParameter(1, 1);
-    double xval, yval, y_allowed_min, y_allowed_max,yvalConvert = 0;
+    double xval, yval, y_allowed_min, y_allowed_max, yvalConvert = 0;
     for(int n = 0; n < 2; n++)
     {
         for(int i = 0; i < myGraphs_vec4->at(n).GetN(); i++)
         { // check that every point is within the allowed min/max curves
-            xval = myGraphs_vec4->at(n).GetX()[i];
-            yval = myGraphs_vec4->at(n).GetY()[i];
-            yvalConvert=(yval-1.)*1000.;
+            xval          = myGraphs_vec4->at(n).GetX()[i];
+            yval          = myGraphs_vec4->at(n).GetY()[i];
+            yvalConvert   = (yval - 1.) * 1000.;
             y_allowed_min = fit_grading_HV_test->Eval(xval) * 0.95 - 20.; // Offset, da bei kleinen Werten der relative Fehler größer sein kann
             y_allowed_max = fit_grading_HV_test->Eval(xval) * 1.05 + 50.;
 
@@ -568,9 +568,9 @@ void SEHTester::ExternalTestBiasVoltage(std::string powerSupplyId, std::string c
 
                 // cout << "WARNING: HV test is bad at HV = " << xval << " V: " << yval << " V, allowed is " << y_allowed_min << " - " << y_allowed_max << " ." << endl;
             }
-            else{
+            else
+            {
                 LOG(DEBUG) << BOLDGREEN << "DEBUG: HV test is good at HV = " << xval << " V: " << yvalConvert << " V, allowed is " << y_allowed_min << " - " << y_allowed_max << " ." << RESET;
-
             }
         }
     }
