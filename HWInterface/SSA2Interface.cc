f8da3f15aa7909515d349ecd2222abf57df723b3
Module Testing <modtest@lenny01.fnal.gov>
2022-10-17 09:21:06 -0500

    Younes fixes for SSA2

--- HWInterface/SSA2Interface.cc
+++ HWInterface/SSA2Interface.cc
@@ -219,7 +219,7 @@ bool SSA2Interface::WriteChipAllLocalReg(ReadoutChip* pChip, const std::string& 
         cItem.fValue  = (cRegName == "strip") ? cMaskValue : 0xFF;
         cRegItems.push_back(cItem);
     }
-    cSuccess = fBoardFW->MultiRegisterWrite(pChip, cRegItems, pVerifLoop);
+    cSuccess = fBoardFW->MultiRegisterWrite(pChip, cRegItems, false);
     if(!cSuccess)
     {
         LOG(INFO) << BOLDRED << "Failed to write to one of these registers" << RESET;
@@ -245,7 +245,7 @@ bool SSA2Interface::WriteChipAllLocalReg(ReadoutChip* pChip, const std::string& 
         // LOG(INFO) << BOLDBLUE << "Setting register " << dacName.str() << " to " << cItem.fValue << RESET;
         cRegItems.push_back(cItem);
     }
-    cSuccess = cSuccess && fBoardFW->MultiRegisterWrite(pChip, cRegItems, pVerifLoop);
+    cSuccess = cSuccess && fBoardFW->MultiRegisterWrite(pChip, cRegItems, false);
     // write mask
     cRegItems.clear();
     cMaskValue = 0xFF;
@@ -256,7 +256,7 @@ bool SSA2Interface::WriteChipAllLocalReg(ReadoutChip* pChip, const std::string& 
         cItem.fValue  = cMaskValue;
         cRegItems.push_back(cItem);
     }
-    cSuccess = cSuccess && fBoardFW->MultiRegisterWrite(pChip, cRegItems, pVerifLoop);
+    cSuccess = cSuccess && fBoardFW->MultiRegisterWrite(pChip, cRegItems, false);
     return cSuccess;
 }
 
