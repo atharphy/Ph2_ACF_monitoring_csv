4aca8ca93ad51c4102dbb1d8e6c190f777e7f9c2
Alkis Papadopoulos <alkiviadis.papadopoylos@cern.ch>
2022-11-14 19:51:10 +0100

    Minor

--- HWInterface/RD53Interface.cc
+++ HWInterface/RD53Interface.cc
@@ -98,7 +98,7 @@ uint16_t RD53Interface::ReadChipReg(Chip* pChip, const std::string& regName)
             return regReadback[0].second;
     }
 
-    LOG(ERROR) << BOLDRED << "Empty register readback FIFO after " << BOLDYELLOW << nAttempts << BOLDRED " attempts" << RESET;
+    LOG(ERROR) << BOLDRED << "Empty register (" << BOLDYELLOW << regName << BOLDRED << ") readback FIFO after " << BOLDYELLOW << nAttempts << BOLDRED " attempts" << RESET;
 
     return 0;
 }
