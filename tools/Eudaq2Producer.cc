be24d858f74e38d262cf41ee2e2a668f3815c38a
Younes Otarid <yotarid@cern.ch>
2022-11-20 18:44:08 +0100

    [Eudaq2Producer] Reerting minor debug change

--- tools/Eudaq2Producer.cc
+++ tools/Eudaq2Producer.cc
@@ -595,8 +595,8 @@ void Eudaq2Producer::ConvertToSubEvent(const BeBoard* pBoard, const Event* pPh2E
         {
             uint8_t cOpticalGroupId = cOpticalGroup->getId();
             // Assign an Id to each sensor
-            uint8_t cPixelSensorId = 0;
-            uint8_t cStripSensorId = 1;
+            uint8_t cPixelSensorId = 2 * cOpticalGroupId;
+            uint8_t cStripSensorId = 2 * cOpticalGroupId + 1;
             // Data containers
             uint16_t             cNPixelHit = 0, cNStripHit = 0;
             uint16_t             cPixelDataOffset = 0, cStripDataOffset = 0;
