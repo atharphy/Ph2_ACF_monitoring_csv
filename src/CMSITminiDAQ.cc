8e305376570e35b862a39b0430172692897064e6
Mauro <mauro.dinardo@cern.ch>
2022-12-17 01:47:05 +0100

    Minor

--- src/CMSITminiDAQ.cc
+++ src/CMSITminiDAQ.cc
@@ -52,7 +52,6 @@ void readBinaryData(const std::string& binaryFile, SystemController& mySysCntr, 
     std::vector<uint32_t> data;
 
     RD53Event::ForkDecodingThreads();
-    RD53Shared::resetDefaultFloat();
 
     LOG(INFO) << BOLDMAGENTA << "@@@ Decoding binary data file @@@" << RESET;
     mySysCntr.addFileHandler(binaryFile, 'r');
