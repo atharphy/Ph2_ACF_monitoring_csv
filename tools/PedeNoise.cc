383172147083b6500939b3944cbe6341b8048a89
Module Testing <modtest@lenny01.fnal.gov>
2023-01-11 13:31:43 -0600

    adding pedenoise bugfix

--- tools/PedeNoise.cc
+++ tools/PedeNoise.cc
@@ -777,6 +777,11 @@ void PedeNoise::extractPedeNoise()
                             auto  cType = chip->getFrontEndType();
                             if(cType == FrontEndType::CBC3 || cType == FrontEndType::SSA || cType == FrontEndType::SSA2)
                             {
+				if(mStripIt == fSCurveStripOccupancyMap.rend())
+				{
+					mStripIt--;
+					continue;
+				}
                                 previousOccupancy = (previousStripIterator)
                                                         ->second->at(board->getIndex())
                                                         ->at(opticalGroup->getIndex())
@@ -790,6 +795,11 @@ void PedeNoise::extractPedeNoise()
                             }
                             else if(cType == FrontEndType::MPA || cType == FrontEndType::MPA2)
                             {
+				if(mPixelIt == fSCurvePixelOccupancyMap.rend())
+				{
+					mPixelIt--;
+					continue;
+				}
                                 previousOccupancy = (previousPixelIterator)
                                                         ->second->at(board->getIndex())
                                                         ->at(opticalGroup->getIndex())
