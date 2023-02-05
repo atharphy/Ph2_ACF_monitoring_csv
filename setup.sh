b93d87e7ba3cce443a0ff8baabb6a0a6bf636134
Lawrence Lee Jr <lawrence.lee.jr@cern.ch>
2023-02-05 03:22:44 +0100

    Changing python aliases to explicitly use python3

--- setup.sh
+++ setup.sh
@@ -32,8 +32,8 @@ export CACTUSINCLUDE=$CACTUSROOT/include
 ##########
 # PYTHON #
 ##########
-alias PythonController.py="python ${PH2ACF_BASE_DIR}/pythonUtils/PythonController.py"
-alias fpgaconfig.py="python ${PH2ACF_BASE_DIR}/pythonUtils/fpgaconfig.py"
+alias PythonController.py="python3 ${PH2ACF_BASE_DIR}/pythonUtils/PythonController.py"
+alias fpgaconfig.py="python3 ${PH2ACF_BASE_DIR}/pythonUtils/fpgaconfig.py"
 
 ########
 # ROOT #
