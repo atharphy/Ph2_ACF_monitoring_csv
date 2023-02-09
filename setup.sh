00d6b2b5b0cb6de277521cc7168e154673072765
Lawrence Lee Jr <lawrence.lee.jr@cern.ch>
2023-02-09 18:31:40 +0100

    Update TC_USB to new ref. Should fix al9 issue.

--- setup.sh
+++ setup.sh
@@ -55,7 +55,7 @@ export AMC13DIR=$CACTUSINCLUDE/amc13
 export POWERSUPPLYDIR=$EXTERNAL_TOOLS_BASE_DIR/power_supply
 
 # These are git references for the dependencies that are included via CMake ExternalProjects
-export PH2_TCUSB_REF=9c39f0f4082f8db6a6788baf3567f55631b53f16
+export PH2_TCUSB_REF=a60e057b2c4f4603cdc6b42e300ad07de132cd81
 export EUDAQ_REF=ac59b87fca12806d775e95df2d253c3bf96420ee
 export PYBIND11_REF=v2.9.2
 
