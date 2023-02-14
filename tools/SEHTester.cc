eecfbe29db584ce82c64ab090eee62d8d36a7cc2
Alexander Pauls <alexander.pauls@rwth-aachen.de>
2023-02-14 14:32:47 +0100

    restoring longer test routines

--- tools/SEHTester.cc
+++ tools/SEHTester.cc
@@ -85,7 +85,10 @@ void SEHTester::RampPowerSupply(std::string powerSupplyId, std::string channelId
     float cVolts = 0;
     float I_SEH;
     float U_SEH;
-    float cVoltages[] = {4.6, 4.8, 5., 5.2, 5.4, 6., 7., 8., 9., 9.5, 9.6, 9.7, 9.8, 9.9, 10., 10.1, 10.2, 10.3, 10.4, 10.5, 10., 9., 8., 7., 6.8, 6.6, 6.4, 6.2, 6.0, 5, 4.8, 4.6};
+    float cVoltages[] = {0.,  0.5,  1.,   1.5,  2.,   2.5,  3.,  3.5, 4.0, 4.2, 4.4, 4.6, 4.8, 5.,  5.2, 5.4, 6.,  7.,  8.,  9.,  9.5, 9.6, 9.7, 9.8, 9.9,
+                         10., 10.1, 10.2, 10.3, 10.4, 10.5, 10., 9.,  8.,  7.,  6.8, 6.6, 6.4, 6.2, 6.0, 5,   4.8, 4.6, 4.4, 4.2, 4.,  3.0, 2.0, 1.0, 0.};
+
+    // float cVoltages[] = {4.6, 4.8, 5., 5.2, 5.4, 6., 7., 8., 9., 9.5, 9.6, 9.7, 9.8, 9.9, 10., 10.1, 10.2, 10.3, 10.4, 10.5, 10., 9., 8., 7., 6.8, 6.6, 6.4, 6.2, 6.0, 5, 4.8, 4.6};
     for(auto& voltage: cVoltages)
     // while(cVolts < 10.01)
     {
@@ -454,7 +457,7 @@ void SEHTester::ExternalTestBiasVoltage(std::string powerSupplyId, std::string c
     setVoltageMessage = "TurnOn,PowerSupplyId:" + powerSupplyId + ",ChannelId:" + channelId;
     fPowerSupplyClient->sendAndReceivePacket(setVoltageMessage);
     std::this_thread::sleep_for(std::chrono::milliseconds(10000));
-    for(int cHvSet = 0; cHvSet <= 1000; cHvSet += 200)
+    for(int cHvSet = 0; cHvSet <= 1000; cHvSet += 100)
     {
         // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 #ifdef __TCP_SERVER__
@@ -784,11 +787,12 @@ void SEHTester::TestEfficiency(uint32_t pMinLoadValue, uint32_t pMaxLoadValue, u
         cEfficiencyValVect.clear(), cU2v5ValVect.clear(), cIoutLValVect.clear();
         cSideValVect.clear(), cUinValVect.clear(), cIoutValVect.clear();
         uint32_t cStep = 0;
-        if(cSide == "both") { cStep = pStep; }
-        else
-        {
-            cStep = 2 * pStep;
-        }
+        cStep = pStep;
+        // if(cSide == "both") { cStep = pStep; }
+        // else
+        // {
+        //     cStep = 2 * pStep;
+        // }
         for(int cLoadValue = pMinLoadValue; cLoadValue <= (int)pMaxLoadValue; cLoadValue += cStep)
         {
             float I_SEH;
