262f3e866e46b077a9ba1a664e04ca04edac1744
Fabio Ravera <fabio.ravera@cern.ch>
2023-03-14 10:45:29 -0500

    Minor

--- src/supervisor.cc
+++ src/supervisor.cc
@@ -326,6 +326,7 @@ int main(int argc, char* argv[])
                 }
                 case STOPPED:
                 {
+                    usleep(2e6);
                     theDQMInterface.stopProcessingData();
                     usleep(5e6);
                     theMiddlewareInterface.halt();
