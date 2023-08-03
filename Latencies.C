73995a6bd2cce0ee74e9a18aa9dff898fa0b941a
iqrasohail <iqrasohail@cern.ch>
2023-08-03 14:10:46 -0500

    useful plotters

---
+++ Latencies.C
@@ -0,0 +1,41 @@
+#include <TH1F.h>
+#include <TFile.h>
+#include <TCanvas.h>
+#include <TSystem.h>
+#include <TLegend.h>
+#include <TStyle.h>
+#include <TGraph.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+void Latencies(int runNumber)
+{
+    TCanvas* C  = new TCanvas("Canvas", "", 1200, 1000);
+    C->Divide(2,3);
+    std::string directoryPath = "Results/OT_ModuleTest_ModuleOT_Run";
+    std::string fileName = "Hybrid.root";
+    std::string filePath = directoryPath  + std::to_string(runNumber) + "/" + fileName;
+    TFile* file = TFile::Open(filePath.c_str(), "READ");
+    std::cout << "Processing run number: " << runNumber<< std::endl;
+    const int numberOfHybrids = 2;
+    for(unsigned int iHybrid=0; iHybrid<numberOfHybrids; iHybrid++)
+    {
+        std::string hybridNumberStr = std::to_string(iHybrid);
+
+        TH1F* h     = (TH1F*)file->Get(("Detector/Board_0/OpticalGroup_0/Hybrid_"+hybridNumberStr+"/D_B(0)_O(0)_LatencyValueS0_Hybrid("+hybridNumberStr+")").c_str());
+        TH1F* h1    = (TH1F*)file->Get(("Detector/Board_0/OpticalGroup_0/Hybrid_"+hybridNumberStr+"/D_B(0)_O(0)_LatencyValueS1_Hybrid("+hybridNumberStr+")").c_str());
+        TH1F* h2    = (TH1F*)file->Get(("Detector/Board_0/OpticalGroup_0/Hybrid_"+hybridNumberStr+"/D_B(0)_O(0)_StubValue_Hybrid("+hybridNumberStr+")").c_str());
+        C->cd(iHybrid +1);
+        h->Draw();
+        C->cd(iHybrid +3);
+        h1->Draw();
+        C->cd(iHybrid +5);
+        h2->Draw();
+        //C->cd(iHybrid +7); 
+        //h3->Draw();
+        C->Update();
+    }
+}
+//}
+
