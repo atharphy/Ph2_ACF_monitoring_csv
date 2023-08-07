1827898adf40bb4c4b7984a4502a417b47b44be1
ModTest <modtest@lenny01.fnal.gov>
2023-08-07 16:01:55 -0500

    fixed DE-SYNC issue to before https://gitlab.cern.ch/cms_tk_ph2/Ph2_ACF/-/commit/8fa94b53c930d77c5a5bed8465999b91607b018e#cf8117e510de077ae185f65e5ccf8dc4f240a8d8

--- tools/LinkAlignmentOT.cc
+++ tools/LinkAlignmentOT.cc
@@ -1044,11 +1044,11 @@ bool LinkAlignmentOT::AlignStubPackage(BeBoard* pBoard)
                         cSyncThisLink      = (cBxIdsFirst == cBxIdsSecond);
                         LOG(INFO) << BOLDRED << " cBxIdsFirst " << cBxIds[cIter.second[0]].size() << " cBxIdsSecond "<< cBxIds[cIter.second[1]].size() << " cSyncThisLink " << cSyncThisLink << RESET;
                         if(cSyncThisLink) LOG(INFO) << BOLDGREEN << "Sync on Link#" << +cIter.first << " between Hybrid#" << +cIter.second[0] << " and Hybrid#" << +cIter.second[1] << RESET;
-                        // if in sync.. add first hybrid id to list
-                        if(cSyncThisLink) { cIdsToCompare.push_back(cIter.second[0]); }
-                        else
-                            LOG(INFO) << BOLDRED << "\t..FAILED sync on Link#" << +cIter.first << " between Hybrid#" << +cIter.second[0] << " and Hybrid#" << +cIter.second[1] << RESET;
                     }
+                    // if in sync.. add first hybrid id to list
+                    if(cSyncThisLink) { cIdsToCompare.push_back(cIter.second[0]); }
+                    else
+                        LOG(INFO) << BOLDRED << "\t..FAILED sync on Link#" << +cIter.first << " between Hybrid#" << +cIter.second[0] << " and Hybrid#" << +cIter.second[1] << RESET;   
                 }
                 LOG(INFO) << BOLDRED << " cIdsToCompare.size() " << cIdsToCompare.size() << " cHybridIdsMap.size() " << cHybridIdsMap.size() << RESET;
                 // if all the links are synchronous then.. check if we are
