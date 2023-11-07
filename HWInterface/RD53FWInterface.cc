d1483655afb1797598a89a11680af77bbd3c1d1c
Mauro <mauro.dinardo@cern.ch>
2023-11-07 15:39:49 +0100

    Added gtx polarity strobe

--- HWInterface/RD53FWInterface.cc
+++ HWInterface/RD53FWInterface.cc
@@ -308,6 +308,7 @@ void RD53FWInterface::ConfigureFromXML(const BeBoard* pBoard)
         RegManager::WriteStackReg(cVecReg);
         RD53FWInterface::SendBoardCommandWithStrobe("user.ctrl_regs.fast_cmd_reg_1.load_config");
         RD53FWInterface::SendBoardCommandWithStrobe("user.ctrl_regs.ext_tlu_reg2.dio5_load_config");
+        RegManager::WriteStackReg({{"user.ctrl_regs.gtx_rx_polarity.cmd_strobe", 1}, {"user.ctrl_regs.fast_cmd_reg_1.cmd_strobe", 0}});
     }
 
     LOG(INFO) << BOLDBLUE << "\t--> Done" << RESET;
