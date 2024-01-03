81a91b8e5b1b1a71250eab9d858ff48c6177f934
Irene Zoi <irene.zoi@cern.ch>
2024-01-03 14:03:25 -0600

    chage cic fast command sampling edge if resync does not work the first time

--- System/SystemController.cc
+++ System/SystemController.cc
@@ -717,10 +717,23 @@ void SystemController::InitializeOT(BeBoard* pBoard)
 
                 if(fCicInterface->GetResyncRequest(cCic))
                 {
-                    LOG(INFO) << BOLDRED << "FAILED to clear CIC ReSync request on Board id " << +pBoard->getId() << " OpticalGroup id" << +cOpticalGroup->getId() << " Hybrid id " << +cHybrid->getId()
-                              << " --- Hybrid will be disabled" << RESET;
-                    ExceptionHandler::getInstance()->disableHybrid(pBoard->getId(), cOpticalGroup->getId(), cHybrid->getId());
-                    continue;
+                    LOG(INFO) << RED << "FAILED to clear CIC ReSync request on Board id " << +pBoard->getId() << " OpticalGroup id" << +cOpticalGroup->getId() << " Hybrid id " << +cHybrid->getId()
+                              << " --- trying to change fast command sampling edge" << RESET;
+                    
+                    // Change the sampling edge of the fast command and then resync again
+                    uint8_t cCicEdge = fCicInterface->ReadFCMDEdge(cCic);
+                    cCicEdge = (cCicEdge == 0 )? 1 : 0;
+                    fCicInterface->ConfigureFCMDEdge(cCic, cCicEdge);
+                    cCicEdge = fCicInterface->ReadFCMDEdge(cCic);
+                    fBeBoardInterface->ChipReSync(pBoard);
+
+                    if(fCicInterface->GetResyncRequest(cCic))
+                    {
+                        LOG(INFO) << BOLDRED << "FAILED to clear CIC ReSync request on Board id " << +pBoard->getId() << " OpticalGroup id" << +cOpticalGroup->getId() << " Hybrid id " << +cHybrid->getId()
+                                << " --- Hybrid will be disabled" << RESET;
+                        ExceptionHandler::getInstance()->disableHybrid(pBoard->getId(), cOpticalGroup->getId(), cHybrid->getId());
+                        continue;
+                    }                    
                 }
             }
         }
