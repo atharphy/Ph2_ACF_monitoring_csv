eb7a77f035e48cb0d4dfc9c517467a4802152d9d
Irene Zoi <irene.zoi@cern.ch>
2024-01-04 15:25:59 -0600

    remove PSv1 debugging stuff

--- System/SystemController.cc
+++ System/SystemController.cc
@@ -861,37 +861,6 @@ void SystemController::ModuleStartUpPS(const OpticalGroup* pOpticalGroup)
             {
                 static_cast<D19clpGBTInterface*>(flpGBTInterface)->cicReset(clpGBT, 0);
             }
-            bool setSSACurrent = true;
-            for(auto cChip: *cHybrid)
-            {
-                if(cChip->getFrontEndType() == FrontEndType::MPA2)
-                {
-                    setSSACurrent = false;
-                    break;
-                }
-            }
-            if(setSSACurrent)
-            {
-                bool cSkipSSA3 = true; // eventually this needs to be set in the xml somewhere
-                for(uint8_t cSSAId = 0; cSSAId < 8; cSSAId++)
-                {
-                    if(cSkipSSA3 && cSSAId == 3) continue;
-                    SSA*    cSSA          = new SSA(cHybrid->getBeBoardId(),
-                                        cHybrid->getFMCId(),
-                                        cHybrid->getOpticalGroupId(),
-                                        cHybrid->getId(),
-                                        cSSAId,
-                                        0,
-                                        0,
-                                        std::string(std::getenv("PH2ACF_BASE_DIR")) + "/settings/SSAFiles/SSA.txt");
-                    uint8_t cSLVSdriveSSA = cSSA->getReg("SLVS_pad_current");
-                    cSSA->setOptical(cHybrid->isOptical());
-                    cSSA->setMasterId(cHybrid->getMasterId());
-                    LOG(INFO) << BOLDMAGENTA << "SSA " << +cSSAId << " current set to " << +cSLVSdriveSSA << "" << RESET;
-                    auto cRegItem = cSSA->getRegItem("SLVS_pad_current");
-                    (fBeBoardInterface->getFirmwareInterface())->SingleRegisterWrite(cSSA, cRegItem, false);
-                }
-            }
 
         } // hybrid
     }     // lpGBT part ... resets + clocks
