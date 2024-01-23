a34a2cd09997fd9cb1fb4549d102f361c66f10d5
Alexander Pauls <alexander.pauls@rwth-aachen.de>
2024-01-23 17:16:18 +0100

    hotfix metadata remove

--- tools/Tool.cc
+++ tools/Tool.cc
@@ -307,11 +307,14 @@ void Tool::SoftDestroy()
         delete fOfStream;
         fOfStream = nullptr;
     }
+#if defined(__TCUSB__)
+#else
     if(fMetadataHandler != nullptr)
     {
         delete fMetadataHandler;
         fMetadataHandler = nullptr;
     }
+#endif
 #ifdef __USE_ROOT__
     if(fResultFile != nullptr)
     {
