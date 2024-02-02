923c73a2babbef8dad0d31e45297a0284a6d2ac7
Fabio Ravera <fabio.ravera@cern.ch>
2024-02-02 09:39:24 -0600

    Run formatAll

--- Parser/FileDumper.cc
+++ Parser/FileDumper.cc
@@ -15,14 +15,15 @@
 
 using namespace Ph2_HwDescription;
 
-FileDumper::FileDumper(const std::string& outputDirectory) {
+FileDumper::FileDumper(const std::string& outputDirectory)
+{
     fOutputDirectory = std::string(getenv("PH2ACF_BASE_DIR")) + "/" + outputDirectory + "/";
-    //Overwrite if GUI is involved
+    // Overwrite if GUI is involved
     if(std::getenv("GIPHT_RESULT_FOLDER"))
     {
-        LOG(INFO) << "OT Module GUI (GIPHT) result directory environmental variable set: " << std::getenv("GIPHT_RESULT_FOLDER")<< RESET;
+        LOG(INFO) << "OT Module GUI (GIPHT) result directory environmental variable set: " << std::getenv("GIPHT_RESULT_FOLDER") << RESET;
         fOutputDirectory = std::getenv("GIPHT_RESULT_FOLDER");
-        LOG (INFO) << "Use "<< fOutputDirectory << " for file dump" << RESET;
+        LOG(INFO) << "Use " << fOutputDirectory << " for file dump" << RESET;
     }
 }
 
