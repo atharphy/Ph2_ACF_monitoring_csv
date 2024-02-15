cab5554b01546179abc9ce8b7c10b6c59ad438b6
cms-thinkcenter-desktop <cms@static-3-226.fi.infn.it>
2024-02-15 17:46:22 +0100

    Error Message

--- HWInterface/RD53lpGBTInterface.cc
+++ HWInterface/RD53lpGBTInterface.cc
@@ -185,7 +185,6 @@ bool RD53lpGBTInterface::ConfigureChip(Chip* pChip, bool pVerify, uint32_t pBloc
     // #######################################
     // # Properly configure lpGBT to use ADC #
     // #######################################
-<<<<<<< HEAD
     
     uint32_t pChipId = pChip->getId();
     std::string pFileName = expandEnvironmentVariables("${PH2ACF_BASE_DIR}/settings/lpGBTFiles/lpgbt_calibration.csv");
@@ -204,12 +203,11 @@ bool RD53lpGBTInterface::ConfigureChip(Chip* pChip, bool pVerify, uint32_t pBloc
     TuneVrefControlLib(static_cast<lpGBT*>(pChip));
     
     AutoTuneVref(static_cast<lpGBT*>(pChip));
-=======
+
     lpGBTInterface::LoadCalibrationData(static_cast<lpGBT*>(pChip), pChip->getId());
     lpGBTInterface::EstimateTemperatureUncalibVref(static_cast<lpGBT*>(pChip));
     lpGBTInterface::TuneVrefControlLib(static_cast<lpGBT*>(pChip));
     lpGBTInterface::AutoTuneVref(static_cast<lpGBT*>(pChip));
->>>>>>> 531ca4ad94d9fb6107b972638b00e356c2f633e6
 
     return true;
 }
