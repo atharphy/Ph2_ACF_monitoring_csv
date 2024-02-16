fbea843e755a0f3e4f344cdd08774431c32c5dc8
cms-thinkcenter-desktop <cms@static-3-226.fi.infn.it>
2024-02-16 10:30:24 +0100

    notation fixes

--- HWInterface/RD53lpGBTInterface.cc
+++ HWInterface/RD53lpGBTInterface.cc
@@ -186,25 +186,18 @@ bool RD53lpGBTInterface::ConfigureChip(Chip* pChip, bool pVerify, uint32_t pBloc
     // # Properly configure lpGBT to use ADC #
     // #######################################
     
-    uint32_t pChipId = pChip->getId();
+    //std::string pFileName= pChip->getFileName();
     std::string pFileName = expandEnvironmentVariables("${PH2ACF_BASE_DIR}/settings/lpGBTFiles/lpgbt_calibration.csv");
     
     std::ifstream stream(pFileName);
     if (!stream)
     {
         LOG(ERROR) << BOLDRED << "Error: The filename " << pFileName << " does not exist!" << RESET;
+        //return false;
         exit(EXIT_FAILURE);
     }
 
-    LoadCalibrationData(static_cast<lpGBT*>(pChip),pChipId, pFileName);
-
-    EstimateTemperatureUncalibVref(static_cast<lpGBT*>(pChip));
-    
-    TuneVrefControlLib(static_cast<lpGBT*>(pChip));
-    
-    AutoTuneVref(static_cast<lpGBT*>(pChip));
-
-    lpGBTInterface::LoadCalibrationData(static_cast<lpGBT*>(pChip), pChip->getId());
+    lpGBTInterface::LoadCalibrationData(static_cast<lpGBT*>(pChip), pChip->getId(), pFileName);
     lpGBTInterface::EstimateTemperatureUncalibVref(static_cast<lpGBT*>(pChip));
     lpGBTInterface::TuneVrefControlLib(static_cast<lpGBT*>(pChip));
     lpGBTInterface::AutoTuneVref(static_cast<lpGBT*>(pChip));
