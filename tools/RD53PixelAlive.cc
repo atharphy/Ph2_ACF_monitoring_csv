595939385057f047e0d168323ca3b15bdf1b79dc
dinardo <mauro.dinardo@cern.ch>
2024-03-18 17:55:19 +0100

    Minor

--- tools/RD53PixelAlive.cc
+++ tools/RD53PixelAlive.cc
@@ -187,11 +187,11 @@ void PixelAlive::run()
                                 if((doDataIntegrity == 2) && ((statusGood == false) || (RD53Event::decodedEvents.size() == 0)))
                                 {
                                     static_cast<RD53Interface*>(this->fReadoutChipInterface)->InitRD53Uplinks(cChip);
-                                    static_cast<RD53Interface*>(this->fReadoutChipInterface)->MaskAllChannels(cChip, true);
+                                    this->fReadoutChipInterface->MaskAllChannels(cChip, true);
                                     const auto& ele     = std::find(suffix.begin(), suffix.end(), su);
                                     const auto  coreCol = (ele - suffix.begin()) * numberOfBits + i;
 
-                                    LOG(WARNING) << BOLDBLUE << "\t--> Found problematic Core-Column " << BOLDYELLOW << coreCol << BOLDBLUE << " --> I'll try to nail down the problem at pixel level"
+                                    LOG(WARNING) << GREEN << "Found problematic Core-Column " << BOLDYELLOW << coreCol << RESET << GREEN << " --> I'll try to nail down the problem at pixel level"
                                                  << RESET;
 
                                     for(auto row = 0u; row < RD53Shared::firstChip->getNRows(); row++)
@@ -254,9 +254,9 @@ void PixelAlive::run()
                             this->fReadoutChipInterface->WriteChipReg(cChip, regName + su, regValueMap[su]);
                             const auto numberOfBits = RD53Shared::firstChip->getRegMap()[regName + su].fBitSize;
                             uint16_t   mask         = RD53Shared::setBits(numberOfBits);
-                            auto       value        = (std::bitset<RD53Constants::NBIT_MAXREG>(regValueMap[su]) & std::bitset<RD53Constants::NBIT_MAXREG>(mask))
-                                             .to_string()
-                                             .erase(0, RD53Constants::NBIT_MAXREG - numberOfBits);
+                            const auto value        = (std::bitset<RD53Constants::NBIT_MAXREG>(regValueMap[su]) & std::bitset<RD53Constants::NBIT_MAXREG>(mask))
+                                                   .to_string()
+                                                   .erase(0, RD53Constants::NBIT_MAXREG - numberOfBits);
                             bool problems = (regValueMap[su] != mask);
                             LOG(INFO) << (problems ? BOLDRED : BOLDBLUE) << "\t--> " << BOLDYELLOW << regName + su << (problems ? BOLDRED : BOLDBLUE) << " = 0b" << BOLDYELLOW << value
                                       << (problems ? BOLDRED : BOLDBLUE) << " (0 = disabled)" << RESET;
@@ -287,7 +287,7 @@ void PixelAlive::run()
                                 if(badPixelsCounter != 0)
                                 {
                                     static_cast<RD53*>(cChip)->copyMaskToDefault("en");
-                                    LOG(WARNING) << BOLDRED << "\t--> Found " << BOLDYELLOW << badPixelsCounter << BOLDRED << " bad pixel(s) in this region --> masked" << RESET;
+                                    LOG(WARNING) << BOLDRED << "\t\t--> Found " << BOLDYELLOW << badPixelsCounter << BOLDRED << " bad pixel(s) in this region --> masked" << RESET;
                                 }
                             }
                         }
