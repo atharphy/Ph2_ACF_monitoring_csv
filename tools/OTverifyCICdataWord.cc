59b7bee7d2de74cf10a8e28bd72f166d9f6c550a
Fabio Ravera <fabio.ravera@cern.ch>
2024-05-22 10:52:25 -0500

    Added isKickoff flag in settings

--- tools/OTverifyCICdataWord.cc
+++ tools/OTverifyCICdataWord.cc
@@ -26,6 +26,7 @@ void OTverifyCICdataWord::Initialise(void)
     // free the registers in case any
 
     fNumberOfIterations = findValueInSettings<double>("OTverifyCICdataWord_NumberOfIterations", 1000);
+    fIsKickoff          = findValueInSettings<double>("OTverifyBoardDataWord_NumberOfIterations", 0) > 0;
 
     ContainerFactory::copyAndInitHybrid<GenericDataArray<float, NUMBER_OF_CIC_PORTS, 2>>(*fDetectorContainer, fPatternMatchingEfficiencyContainer);
 
@@ -370,7 +371,6 @@ float OTverifyCICdataWord::injectAndMatch2SstubPatterns(ReadoutChip*            
                                                         std::vector<std::pair<uint8_t, int>> stubSeedAndBendingVector)
 {
     size_t numberOfLines      = 5;
-    bool   isKickoff          = true;
     float  matchingEfficiency = 0;
     fReadoutChipInterface->MaskAllChannels(theChip, true);
     static_cast<CbcInterface*>(fReadoutChipInterface)->injectStubs(theChip, stubSeedAndBendingVector);
@@ -423,7 +423,7 @@ float OTverifyCICdataWord::injectAndMatch2SstubPatterns(ReadoutChip*            
     // padding 0s
     thePattern.addToPattern(0x0, 0xF, 4);
 
-    if(isKickoff && theChip->getHybridId() % 2 == 0) thePattern.maskStubFor2Skickoff();
+    if(fIsKickoff && theChip->getHybridId() % 2 == 0) thePattern.maskStubFor2Skickoff();
 
     for(size_t iteration = 0; iteration < fNumberOfIterations; iteration++)
     {
