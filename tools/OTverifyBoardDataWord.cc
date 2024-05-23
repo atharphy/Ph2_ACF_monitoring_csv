de4c6361034755c59d44dca8299438e7fb9f6746
Irene Zoi <irene.zoi@cern.ch>
2024-05-23 15:07:44 -0500

    fixed kickoff flag

--- tools/OTverifyBoardDataWord.cc
+++ tools/OTverifyBoardDataWord.cc
@@ -22,7 +22,7 @@ void OTverifyBoardDataWord::Initialise(void)
     // free the registers in case any
 
     fNumberOfIterations = findValueInSettings<double>("OTverifyBoardDataWord_NumberOfIterations", 1000);
-    fIsKickoff          = findValueInSettings<double>("OTverifyBoardDataWord_NumberOfIterations", 0) > 0;
+    fIsKickoff          = findValueInSettings<double>("isKickoff", 0) > 0;
 
     size_t             numberOfLines = (fDetectorContainer->getFirstObject()->getFirstObject()->getFrontEndType() == FrontEndType::OuterTrackerPS) ? 7 : 6;
     std::vector<float> initialEmptyVector(numberOfLines, 0);
