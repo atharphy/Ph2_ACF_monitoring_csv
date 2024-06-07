012b1957a8a12229003ccfeb247a8097f5a9431b
Irene Zoi <irene.zoi@cern.ch>
2024-06-07 14:41:29 -0500

    formatAll <3

--- miniDAQ/CombinedCalibrationFactory.cc
+++ miniDAQ/CombinedCalibrationFactory.cc
@@ -67,9 +67,7 @@ CombinedCalibrationFactory::CombinedCalibrationFactory()
              OTverifyCICdataWord,
              OTverifyMPASSAdataWord,
              ECVLinkAlignmentOT>("Outer Tracker", "ecv");
-    Register<OTalignLpGBTinputs,
-             OTalignBoardDataWord,
-             OTverifyECVlpGBTCIC>("Outer Tracker", "OTLpGBTtoCICecv");
+    Register<OTalignLpGBTinputs, OTalignBoardDataWord, OTverifyECVlpGBTCIC>("Outer Tracker", "OTLpGBTtoCICecv");
     Register<OTalignLpGBTinputs>("Outer Tracker", "OTalignLpGBTinputs");
     Register<OTalignBoardDataWord>("Outer Tracker", "OTalignBoardDataWord");
     Register<OTalignBoardDataWord, OTverifyBoardDataWord>("Outer Tracker", "OTverifyBoardDataWord");
