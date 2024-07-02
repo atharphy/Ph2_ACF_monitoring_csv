6cfba00dc66cd05b7b39af411b66c1f1aa2fc2e4
dinardo <mauro.dinardo@cern.ch>
2024-07-02 15:06:27 +0200

    Minor

--- tools/RD53Physics.cc
+++ tools/RD53Physics.cc
@@ -106,8 +106,8 @@ void Physics::Stop()
     CalibBase::saveChipRegisters(doUpdateChip);
 
     LOG(INFO) << GREEN << "[Physics::Stop] Stopped" << RESET;
-    LOG(INFO) << BOLDBLUE << "\t--> Total number of recorded bunch crossings: " << BOLDYELLOW << numberOfEventsPerRun << RESET;
-    LOG(INFO) << BOLDBLUE << "\t--> Total number of received triggers (i.e. events): " << BOLDYELLOW << numberOfEventsPerRun / nTRIGxEvent << RESET;
+    LOG(INFO) << BOLDBLUE << "\t--> Total number of recorded events (i.e. bunch crossings): " << BOLDYELLOW << numberOfEventsPerRun << RESET;
+    LOG(INFO) << BOLDBLUE << "\t--> Total number of received triggers: " << BOLDYELLOW << numberOfEventsPerRun / nTRIGxEvent << RESET;
     LOG(INFO) << BOLDBLUE << "\t--> Total number of corrupted bunch crossings: " << BOLDYELLOW << std::setprecision(3) << corruptedEventCounter << " ("
               << 1. * corruptedEventCounter / numberOfEventsPerRun * 100. << "%)" << std::setprecision(-1) << RESET;
 }
@@ -246,7 +246,7 @@ void Physics::fillDataContainer(BeBoard& theBoard)
 
         if(RD53Event::EvtErrorHandler(static_cast<RD53Event*>(event)->eventStatus) == false)
         {
-            LOG(ERROR) << BOLDBLUE << "\t--> Corrupted event n. " << BOLDYELLOW << evtCounter << RESET;
+            LOG(ERROR) << BOLDBLUE << "\t--> Corrupted bunch crossing n. " << BOLDYELLOW << evtCounter << RESET;
             corruptedEventCounter++;
             RD53Event::PrintEvents({*static_cast<RD53Event*>(event)});
         }
