4d92c70d5c413e750689d83253c240264dbdee9d
Colby Thompson <cthom183@vols.utk.edu>
2024-07-29 14:42:57 -0500

    Updated DQMHistogramPedeNoise.cc to properly fit the S-curve of PS modules

--- Utils/Utilities.cc
+++ Utils/Utilities.cc
@@ -111,6 +111,8 @@ double MyErf(double* x, double* par)
     return fitval;
 }
 
+double MyErfc(double* x, double* par) { return 1 - MyErf(x, par); }
+
 double MyGammaSignal(double* x, double* par)
 {
     double VCth = x[0];
