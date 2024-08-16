7ad0079c0ad267c9b93d30fa34ac441189ef4381
Stefan Maier <stefan.maier@cern.ch>
2024-08-16 09:11:14 +0200

    first try

--- tools/OTVTRXLightOff.cc
+++ tools/OTVTRXLightOff.cc
@@ -31,6 +31,13 @@ void OTVTRXLightOff::Running()
 void OTVTRXLightOff::TurnOffLight()
 {
     // this->ConfigureHw();
+    if(fDetectorMonitor != nullptr)
+    {
+        fDetectorMonitor->stopRunning();
+        fDetectorMonitor->waitForMonitorToStop();
+    }
+    delete fDetectorMonitor;
+    fDetectorMonitor = nullptr;
     for(const auto cBoard: *fDetectorContainer)
     {
         D19cFWInterface*      pInterface        = static_cast<D19cFWInterface*>(fBeBoardFWMap.find(cBoard->getId())->second);
