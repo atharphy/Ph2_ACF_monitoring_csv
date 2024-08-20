81d4dd63985f760a9a232e779008d812229d91e7
Kuldeep Pal <kpal@cern.ch>
2024-08-20 12:08:42 +0530

    copied theEfficiency calculations from OTalignLpGBTForBypass, and doing runSmallEcv()

--- tools/OTCBCtoCICecv.cc
+++ tools/OTCBCtoCICecv.cc
@@ -45,7 +45,7 @@ void OTCBCtoCICecv::Running()
     //printCICStrengthAndPhase();
     //itrOverCBCStrength();
     LOG(INFO) << BOLDRED << "========== Starting OTCBC2CICalignment test ==========" << RESET;
-    itrOverCBCStrength();
+    runSmallEcv();
     LOG(INFO) << BOLDRED << "========== End OTCBC2CICalignment test ==========" << RESET;
     LOG(INFO) << "Done with OTCBCtoCICecv.";
     Reset();
@@ -296,6 +296,212 @@ void OTCBCtoCICecv::itrOverCBCStrength()
                         LOG(INFO) << "kpal: CBC number: " << theCBC->getId() << " BetaMult&SLVS: 0x" << std::hex << cReg << std::dec << " pVerifyBit: " << pVerifyBit << RESET;
                     }
                 }
-    calcEfficiencyBypassingCIC();
+    //calcEfficiencyBypassingCIC();
     }
+}
+
+void OTCBCtoCICecv::runSmallEcv()
+{
+    prepareForLpGBTalignment2Sstubs();
+    prepareForLpGBTalignment2SL1();
+    uint8_t numberOfLines = 4;
+
+    for(uint8_t phyPort = 0; phyPort < 12; ++phyPort)
+    {
+        setCICBypass(phyPort);
+        DetectorDataContainer matchingEfficiencyContainer;
+        ContainerFactory::copyAndInitHybrid<GenericDataArray<float, 4>>(*fDetectorContainer, matchingEfficiencyContainer);
+        for(auto theBoard: *fDetectorContainer)
+        {
+            auto theFWinterface = static_cast<D19cFWInterface*>(fBeBoardInterface->getFirmwareInterface(theBoard));
+            for(auto theOpticalGroup: *theBoard)
+            {
+                for(auto theHybrid: *theOpticalGroup)
+                {
+                    std::vector<std::vector<uint32_t>> phyPortDataVector(numberOfLines);
+                    fBeBoardInterface->WriteBoardReg(
+                        fDetectorContainer->getObject(theHybrid->getBeBoardId()), "fc7_daq_cnfg.physical_interface_block.slvs_debug.hybrid_select", theHybrid->getId());
+                    fBeBoardInterface->WriteBoardReg(fDetectorContainer->getObject(theHybrid->getBeBoardId()), "fc7_daq_cnfg.physical_interface_block.slvs_debug.chip_select", 0);
+
+                    for(size_t iteration = 0; iteration < fNumberOfIterations; iteration++)
+                    {
+                        if(phyPort >= 10)
+                        {
+                            fBeBoardInterface->WriteBoardReg(theBoard, "fc7_daq_ctrl.fast_command_block.control.stop_trigger", 0x1);
+                            usleep(10);
+                            fBeBoardInterface->WriteBoardReg(theBoard, "fc7_daq_ctrl.fast_command_block.control.start_trigger", 0x1);
+                        }
+                        auto lineOutputVector = theFWinterface->StubDebug(true, numberOfLines, false);
+                        for(uint8_t line = 0; line < numberOfLines; ++line)
+                        {
+                            phyPortDataVector[line].insert(phyPortDataVector[line].end(), lineOutputVector[line].begin(), lineOutputVector[line].end());
+                        }
+                    }
+
+                    for(uint8_t line = 0; line < numberOfLines; ++line)
+                    {
+                        auto& matchingEfficiency =
+                            matchingEfficiencyContainer.getHybrid(theBoard->getId(), theOpticalGroup->getId(), theHybrid->getId())->getSummary<GenericDataArray<float, 5>>()[line];
+                        if(phyPort >= 10) // L1 for 2S case
+                        {
+                            matchingEfficiency = getMatchingEfficiency2SL1(phyPortDataVector[line]);
+                            LOG(INFO) << "kpal: phyPort: " << +phyPort << " line: " << +line << " matchingEfficiency: " << matchingEfficiency << RESET;
+                        }
+                        else
+                        {
+                            uint8_t thePattern;
+                            thePattern = fStubPattern2S[(phyPort * 4 + line) % 5];
+                            auto possiblePatternList = getPossiblePatterns(thePattern, static_cast<D19clpGBTInterface*>(flpGBTInterface)->GetChipRate(theOpticalGroup->flpGBT) == 10);
+                            matchingEfficiency       = countMatchingBits(phyPortDataVector[line], possiblePatternList);
+                            LOG(INFO) << "kpal: phyPort: " << +phyPort << " line: " << +line << " matchingEfficiency: " << matchingEfficiency << RESET;
+                        }
+                    }
+                }
+            }
+            fBeBoardInterface->WriteBoardReg(theBoard, "fc7_daq_ctrl.fast_command_block.control.stop_trigger", 0x1);
+        }
+
+//#ifdef __USE_ROOT__
+//        LOG(INFO) << "Using ROOT to save OTCBCtoCICecv matching efficiency." << RESET;
+//        fDQMHistogramOTCBCtoCICecv.fillMatchingEfficiency(matchingEfficiencyContainer, phyPort);
+//#else
+//        if(fDQMStreamer)
+//        {
+//            LOG(INFO) << "Using DQMStreamer to save OTCBCtoCICecv matching efficiency." << RESET;
+//            ContainerSerialization theMatchingEfficiencySerialization("OTCBCtoCICecvMatchingEfficiency");
+//            theMatchingEfficiencySerialization.streamByHybridContainer(fDQMStreamer, matchingEfficiencyContainer, phyPort);
+//        }
+//#endif
+
+
+    }
+}
+
+void OTCBCtoCICecv::setCICBypass(uint8_t phyPort)
+{
+    for(auto theBoard: *fDetectorContainer)
+    {
+        for(auto theOpticalGroup: *theBoard)
+        {
+            for(auto theHybrid: *theOpticalGroup)
+            {
+                auto& theCic = static_cast<OuterTrackerHybrid*>(theHybrid)->fCic;
+                fCicInterface->SelectOutput(theCic, false);
+                fCicInterface->WriteChipReg(theCic, "MUX_CTRL", 0x10 | phyPort);
+            }
+        }
+    }
+}
+
+void OTCBCtoCICecv::prepareForLpGBTalignment2Sstubs()
+{
+    auto theCbcInterface = static_cast<CbcInterface*>(fReadoutChipInterface);
+    for(auto theBoard: *fDetectorContainer)
+    {
+        for(auto theOpticalGroup: *theBoard)
+        {
+            for(auto theHybrid: *theOpticalGroup)
+            {
+                for(auto theChip: *theHybrid)
+                {
+                    // switch on HitOr
+                    fReadoutChipInterface->WriteChipReg(theChip, "HitOr", 1);
+                    // set PtCut to maximum
+                    fReadoutChipInterface->WriteChipReg(theChip, "PtCut", 14);
+                    // disable cluster cut
+                    fReadoutChipInterface->WriteChipReg(theChip, "ClusterCut", 4);
+                    theCbcInterface->selectLogicMode(theChip, "Sampled", true, true);
+
+                    std::vector<std::pair<std::string, uint16_t>> theRegisterVector;
+                    theRegisterVector.push_back({"Bend7", 0x0A}); // forcing Bend7 (bending = 0) to ouput 0xA
+                    // theRegisterVector.push_back({"Bend8", 0x0C});              // forcing Bend8 (bending = 1) to ouput 0xC
+                    theRegisterVector.push_back({"CoincWind&Offset12", 0x00}); // set stub window offset to 0
+                    theRegisterVector.push_back({"CoincWind&Offset34", 0x00}); // set stub window offset to 0
+                    fReadoutChipInterface->WriteChipMultReg(theChip, theRegisterVector);
+
+                    std::vector<std::pair<uint8_t, int>> stubSeedAndBend{{fStubPattern2S[0], 0}, {fStubPattern2S[1], 0}, {fStubPattern2S[2], 0}};
+                    theCbcInterface->injectStubs(theChip, stubSeedAndBend);
+                }
+            }
+        }
+    }
+}
+
+void OTCBCtoCICecv::prepareForLpGBTalignment2SL1()
+{
+    uint32_t triggerFrequency        = 1000; // do not change or it will not match padding 0s
+    uint8_t  fakeHeaderChannelNumber = 24;
+    fPattern2SL1.clear();
+    fPattern2SL1.addToPattern(0x3, 0x3, 2); // CBC header
+    fPattern2SL1.addToPattern(0x0, 0x0, 2); // error flags
+    fPattern2SL1.addToPattern(0x0, 0x0, 9); // pipe address
+    fPattern2SL1.addToPattern(0x0, 0x0, 9); // L1 counter
+
+    for(uint8_t fakeHeaderChannel = 0; fakeHeaderChannel < fakeHeaderChannelNumber; ++fakeHeaderChannel)
+    {
+        fPattern2SL1.addToPattern(0x1, 0x1, 1); // fake channel header
+    }
+
+    for(uint8_t alternatedChannels = fakeHeaderChannelNumber; alternatedChannels < NCHANNELS; ++alternatedChannels)
+    {
+        fPattern2SL1.addToPattern((alternatedChannels + 1) % 2, 0x1, 1); // enable even numbers
+    }
+
+    uint32_t bitsBetweenConsecutiveTriggers = 40000 / triggerFrequency * 8;
+
+    for(uint16_t paddingZeros = fPattern2SL1.getNumberOfPatternBits(); paddingZeros < bitsBetweenConsecutiveTriggers; ++paddingZeros)
+    {
+        fPattern2SL1.addToPattern(0, 0x1, 1); // padding zeros
+    }
+
+    auto theCbcInterface = static_cast<CbcInterface*>(fReadoutChipInterface);
+    for(auto theBoard: *fDetectorContainer)
+    {
+        std::vector<std::pair<std::string, uint32_t>> registerVector;
+        registerVector.push_back({"fc7_daq_cnfg.fast_command_block.trigger_source", 3});
+        registerVector.push_back({"fc7_daq_cnfg.fast_command_block.triggers_to_accept", 0});
+        registerVector.push_back({"fc7_daq_cnfg.fast_command_block.user_trigger_frequency", triggerFrequency});
+        registerVector.push_back({"fc7_daq_ctrl.fast_command_block.control.load_config", 0x1});
+        fBeBoardInterface->WriteBoardMultReg(theBoard, registerVector);
+        // fBeBoardInterface->WriteBoardReg(theBoard, "fc7_daq_ctrl.fast_command_block.control.start_trigger", 0x1);
+
+        for(auto theOpticalGroup: *theBoard)
+        {
+            for(auto theHybrid: *theOpticalGroup)
+            {
+                for(auto theChip: *theHybrid)
+                {
+                    fReadoutChipInterface->WriteChipReg(theChip, "ClusterCut", 4);
+                    fReadoutChipInterface->WriteChipReg(theChip, "VCth", 1023);
+                    theCbcInterface->selectLogicMode(static_cast<ReadoutChip*>(theChip), "Sampled", true, true);
+
+                    auto cChannelMask = std::make_shared<ChannelGroup<1, NCHANNELS>>();
+                    cChannelMask->disableAllChannels();
+                    for(uint8_t cChannel = 0; cChannel < NCHANNELS; cChannel += 2) cChannelMask->enableChannel(0, cChannel); // generate a hit in every Nth channel
+                    for(uint8_t cChannel = 0; cChannel < fakeHeaderChannelNumber; ++cChannel)
+                        cChannelMask->enableChannel(0, cChannel); // generate a hit in the first 32 channels to create a sort of fake header
+                    fReadoutChipInterface->maskChannelGroup(static_cast<ReadoutChip*>(theChip), cChannelMask);
+                }
+            }
+        }
+    }
+}
+
+float OTCBCtoCICecv::getMatchingEfficiency2SL1(std::vector<uint32_t> inputDataVector)
+{
+    float   totalEfficiency             = 0;
+    uint8_t numberOfWordsPerAcquisition = 10; // 10 32-bit-words per acquisition;
+    float   numberOfAcquisitions        = inputDataVector.size() / numberOfWordsPerAcquisition;
+    for(size_t acquisitionNumber = 0; acquisitionNumber < numberOfAcquisitions; ++acquisitionNumber)
+    {
+        size_t                firstIndex = acquisitionNumber * numberOfWordsPerAcquisition;
+        size_t                lastIndex  = firstIndex + numberOfWordsPerAcquisition;
+        std::vector<uint32_t> singleAcquisitionInputDataVector(inputDataVector.begin() + firstIndex, inputDataVector.begin() + lastIndex);
+        auto                  reorderedSingleAcquisitionInputDataVector = reorderPattern(singleAcquisitionInputDataVector, 1);
+        auto                  maximumEfficiency                         = fPattern2SL1.getNumberOfMatchingBitsForAllBitshifts<320>(reorderedSingleAcquisitionInputDataVector);
+        totalEfficiency += maximumEfficiency;
+    }
+
+    auto numberOfUnmaskedBits = fPattern2SL1.getNumberOfMaskedBits();
+    return totalEfficiency / (numberOfAcquisitions * numberOfUnmaskedBits);
 }
--- tools/OTCBCtoCICecv.h
+++ tools/OTCBCtoCICecv.h
@@ -17,6 +17,7 @@
 // Calibration is not running on the SoC: I need to instantiate the DQM histogrammer here
 #include "DQMUtils/DQMHistogramOTCBCtoCICecv.h"
 #endif
+#include "Utils/PatternMatcher.h"
 
 namespace Ph2_HwDescription
 {
@@ -52,11 +53,24 @@ class OTCBCtoCICecv : public OTCicBypassTest
     void itrOverCBCStrength();
     void                               setCBCshiftRegister();
     void                               runElectricChainValidation();
+    void                               runSmallEcv();
     std::vector<std::vector<uint32_t>> readCICbypassOutput(Ph2_HwDescription::Hybrid* theHybrid, Ph2_HwInterface::D19cFWInterface* theFWinterface, uint8_t phyPort);
 
     uint8_t            fShiftRegisterPattern{0xAA};
     uint32_t           fNumberOfIterations{100};
     std::vector<float> fListOfCBCslvsCurrents{1, 4, 7};
+
+    //////////////////////// From OTalignLpGBTinputsForBypass.cc //////////////////////////
+    void    prepareForLpGBTalignment2Sstubs();
+    void    prepareForLpGBTalignment2SL1();
+    float   getMatchingEfficiency2SL1(std::vector<uint32_t> inputDataVector);
+    void    setCICBypass(uint8_t phyPort);
+
+    uint8_t              fShiftRegisterPatternMPA{0xAA};
+    std::vector<uint8_t> fStubPattern2S{0x33, 0x55, 0xAA, 0xAA, 0xAA}; // last two bytes cannot be changed here
+
+    PatternMatcher fPattern2SL1;
+    ///////////////////////////////////////////////////////////////////////////////////////
 
 #ifdef __USE_ROOT__
     // Calibration is not running on the SoC: Histogrammer is handeld by the calibration itself
