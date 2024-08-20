32cbf1e1f4eddb581e1d135f66687d1f78ce784e
Kuldeep Pal <kpal@cern.ch>
2024-08-20 15:33:47 +0530

    added diagnosticCheckForMatchingEfficiency in DQMHistogramOTCBCtoCICecv

--- DQMUtils/DQMHistogramOTCBCtoCICecv.h
+++ DQMUtils/DQMHistogramOTCBCtoCICecv.h
@@ -57,6 +57,7 @@ class DQMHistogramOTCBCtoCICecv : public DQMHistogramBase
 
     void fillPhaseScanMatchingEfficiency(DetectorDataContainer& thePhaseMatchingEfficiency, uint8_t phase, uint8_t slvsCurrent);
     void fillMatchingEfficiency(DetectorDataContainer& matchingEfficiencyContainer, uint8_t phyPort);
+    void diagnosticCheckForMatchingEfficiency(DetectorDataContainer& matchingEfficiencyContainer, uint8_t phyPort);
 
   private:
     DetectorContainer*                       fDetectorContainer;
