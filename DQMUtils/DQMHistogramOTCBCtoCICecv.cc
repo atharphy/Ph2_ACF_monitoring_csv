072b769d2d01ca80279282dc441d5e7aa9dffc72
Kuldeep Pal <kpal@cern.ch>
2024-08-20 15:33:15 +0530

    added LOG(INFO) in fillMatchingEfficiency

--- DQMUtils/DQMHistogramOTCBCtoCICecv.cc
+++ DQMUtils/DQMHistogramOTCBCtoCICecv.cc
@@ -160,10 +160,18 @@ void DQMHistogramOTCBCtoCICecv::fillMatchingEfficiency(DetectorDataContainer& ma
             for(auto theHybrid: *theOpticalGroup)
             {
                 if(!theHybrid->hasSummary()) continue;
+                LOG(INFO) << "Board " << theBoard->getId() << " OpticalGroup " << theOpticalGroup->getId() << " Hybrid " << theHybrid->getId() << RESET;
                 auto thePhaseScanHistogram =
-                    fPhaseScanMatchingEfficiencies[phyPort].getHybrid(theBoard->getId(), theOpticalGroup->getId(), theHybrid->getId())->getSummary<HistContainer<TH1F>>().fTheHistogram;
+                    fPhaseScanMatchingEfficiencies[phyPort].getHybrid(theBoard->getId(), theOpticalGroup->getId(), theHybrid->getId())->getSummary<HistContainer<TH2F>>().fTheHistogram;
+                LOG(INFO) << "ThePhaseScanHistogram loaded" << RESET;
                 auto theEfficiencyArray = theHybrid->getSummary<GenericDataArray<float, 4>>();
-                for(size_t line = 0; line < 4; ++line) { thePhaseScanHistogram->SetBinContent(line + 1, theEfficiencyArray[line]); }
+                LOG(INFO) << "TheEfficiencyArray loaded" << RESET;
+                for(size_t line = 0; line < 4; ++line)
+                {
+                    thePhaseScanHistogram->SetBinContent(1, line + 1, theEfficiencyArray[line]);
+                    LOG(INFO) << "The efficiency line " << line + 1 << " is " << theEfficiencyArray[line] << RESET;
+                }
+                //LOG(INFO) << "SetBinContent Done" << RESET;
             }
         }
     }
