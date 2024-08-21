fb9e0a5112f57c6e21a5afdb130a3f01f6bc259c
Kuldeep Pal <kpal@cern.ch>
2024-08-21 18:31:10 +0530

    removed commented out stuff from book in DQMHistogramOTCBCtoCICecv

--- DQMUtils/DQMHistogramOTCBCtoCICecv.cc
+++ DQMUtils/DQMHistogramOTCBCtoCICecv.cc
@@ -28,46 +28,6 @@ void DQMHistogramOTCBCtoCICecv::book(TFile* theOutputFile, DetectorContainer& th
     fDetectorContainer = &theDetectorStructure;
     // SoC utilities only - END
 
-//    std::vector<float> listOfCBCslvsCurrents = convertStringToFloatList(findValueInSettings<std::string>(pSettingsMap, "OTCBCtoCICecv_ListOfCBCslvsCurrents", "1, 4, 7"));
-//
-//    uint8_t numberOfCBC         = 8;
-//    uint8_t numberOfLinesPerCBC = 6;
-//
-//    auto setYaxisBinLable = [numberOfCBC, numberOfLinesPerCBC](TH2F* theHistogram)
-//    {
-//        auto theAxis = theHistogram->GetYaxis();
-//        for(uint8_t cbcId = 0; cbcId < numberOfCBC; ++cbcId)
-//        {
-//            for(uint8_t lineId = 0; lineId < numberOfLinesPerCBC; ++lineId)
-//            {
-//                std::string binLabel = Form("CBC%d_", cbcId + 8);
-//                if(lineId == 0)
-//                    binLabel += "L1";
-//                else
-//                    binLabel += Form("Stub%d", lineId - 1);
-//                theAxis->SetBinLabel(cbcId * numberOfLinesPerCBC + lineId + 1, binLabel.c_str());
-//            }
-//        }
-//    };
-//
-//    for(auto slvsCurrent: listOfCBCslvsCurrents)
-//    {
-//        HistContainer<TH2F> phaseScanMatchingEfficiency(Form("CBCtoCICPhaseScan_SLVScurrent_%d", int(slvsCurrent)),
-//                                                        Form("CBC to CIC Phase Scan Matching efficiency - SLVScurrent = %d", int(slvsCurrent)),
-//                                                        15,
-//                                                        -0.5,
-//                                                        14.5,
-//                                                        numberOfCBC * numberOfLinesPerCBC,
-//                                                        -0.5,
-//                                                        numberOfCBC * numberOfLinesPerCBC - 0.5);
-//        phaseScanMatchingEfficiency.fTheHistogram->GetXaxis()->SetTitle("phase");
-//        setYaxisBinLable(phaseScanMatchingEfficiency.fTheHistogram);
-//        phaseScanMatchingEfficiency.fTheHistogram->SetMinimum(0);
-//        phaseScanMatchingEfficiency.fTheHistogram->SetMaximum(1);
-//        phaseScanMatchingEfficiency.fTheHistogram->SetStats(false);
-//        RootContainerFactory::bookHybridHistograms(theOutputFile, theDetectorStructure, fPhaseScanMatchingEfficiencies[slvsCurrent], phaseScanMatchingEfficiency);
-//    }
-
     uint8_t numberOfStubs = 6; //including L1 (as Stub6)
                                //using line would have been more accurate, but that would create confusion with phyPort's line
                                //so L1 is known as Stub6 from hereon
@@ -101,14 +61,6 @@ void DQMHistogramOTCBCtoCICecv::book(TFile* theOutputFile, DetectorContainer& th
         phaseScanMatchingEfficiency.fTheHistogram->SetStats(false);
         RootContainerFactory::bookHybridHistograms(theOutputFile, theDetectorStructure, fPhaseScanMatchingEfficiencies[cicSlvsCurrent], phaseScanMatchingEfficiency);
     }
-
-//        HistContainer<TH1I> bestPhase(Form("LpGBTforCICbypassBestPhase_phyPort%d", phyPort), Form("LpGBT for CIC Bypass best phase - phyPort %d", phyPort), numberOfLines, -0.5, numberOfLines - 0.5);
-//        bestPhase.fTheHistogram->GetXaxis()->SetTitle("line");
-//        for(uint8_t line = 0; line < numberOfLines; ++line) bestPhase.fTheHistogram->GetXaxis()->SetBinLabel(line + 1, Form("Stub%d", line));
-//        bestPhase.fTheHistogram->SetStats(false);
-//        RootContainerFactory::bookHybridHistograms(theOutputFile, theDetectorStructure, fBestPhase[phyPort], bestPhase);
-
-
 }
 
 //========================================================================================================================
