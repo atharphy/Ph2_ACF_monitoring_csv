eeb8fa6f4b074571031398d40a9422f1491b97f7
dinardo <mauro.dinardo@cern.ch>
2025-01-09 21:51:10 +0100

    Minor

--- Parser/FileParser.cc
+++ Parser/FileParser.cc
@@ -1482,15 +1482,15 @@ void FileParser::parseSettings(const std::string& pFilename, SettingsMap& pSetti
                 value.erase(std::remove(value.begin(), value.end(), ' '), value.end());
                 pSettingsMap[theSettingValue] = value;
 
-                os << BOLDRED << SETTING_NODE_NAME << RESET << " -- " << BOLDCYAN << theSettingValue << RESET << ":" << BOLDYELLOW << boost::any_cast<std::string>(pSettingsMap[theSettingValue])
-                   << RESET << std::endl;
+                os << BOLDRED << SETTING_NODE_NAME << RESET << GREEN << " -- " << BOLDCYAN << theSettingValue << RESET << GREEN << ":" << BOLDYELLOW
+                   << boost::any_cast<std::string>(pSettingsMap[theSettingValue]) << RESET << std::endl;
             }
             else
             {
                 pSettingsMap[theSettingValue] = convertAnyDouble(nSetting.first_child().value());
 
-                os << BOLDRED << SETTING_NODE_NAME << RESET << " -- " << BOLDCYAN << theSettingValue << RESET << ":" << BOLDYELLOW << boost::any_cast<double>(pSettingsMap[theSettingValue]) << RESET
-                   << std::endl;
+                os << BOLDRED << SETTING_NODE_NAME << RESET << GREEN << " -- " << BOLDCYAN << theSettingValue << RESET << GREEN << ":" << BOLDYELLOW
+                   << boost::any_cast<double>(pSettingsMap[theSettingValue]) << RESET << std::endl;
             }
         }
     }
@@ -1700,7 +1700,10 @@ void FileParser::parseMonitor(const std::string& pFilename, DetectorMonitorConfi
     std::string silenRunString               = (theMonitorNode.attribute(MONITORING_NODE_SILENTRUN_ATTRIBUTE_NAME) ? theMonitorNode.attribute(MONITORING_NODE_SILENTRUN_ATTRIBUTE_NAME).value() : "0");
 
     if(enableString == "1")
+    {
         theDetectorMonitorConfig.fEnable = true;
+        os << std::endl;
+    }
     else if(enableString == "0")
         theDetectorMonitorConfig.fEnable = false;
     else
@@ -1715,14 +1718,13 @@ void FileParser::parseMonitor(const std::string& pFilename, DetectorMonitorConfi
 
     theDetectorMonitorConfig.fSleepTimeMs = atoi(theMonitorNode.child(MONITORINGSLEEPTIME_NODE_NAME).first_child().value());
 
-    os << std::endl;
-
     for(pugi::xml_node monitorElement = theMonitorNode.child(MONITORINGELEMENT_NODE_NAME); monitorElement; monitorElement = monitorElement.next_sibling())
     {
         const std::string chipName     = monitorElement.attribute(MONITORINGELEMENT_DEVICE_ATTRIBUTE_NAME).value();
         const std::string registerName = monitorElement.attribute(MONITORINGELEMENT_REGISTER_ATTRIBUTE_NAME).value();
         const bool        enable       = convertAnyInt(monitorElement.attribute(MONITORING_NODE_ENABLE_ATTRIBUTE_NAME).value()) != 0;
-        if(enable) os << BOLDRED << "Monitoring" << RESET << " -- " << BOLDCYAN << chipName << RESET << ":" << BOLDYELLOW << "Register " << registerName << RESET << std::endl;
+        if((enableString == "1") && (enable == true))
+            os << BOLDRED << "Monitoring" << RESET << GREEN << " -- " << BOLDCYAN << chipName << RESET << GREEN << ":Register " << BOLDYELLOW << registerName << RESET << std::endl;
         theDetectorMonitorConfig.addElementToMonitor(chipName, registerName, enable);
     }
 
