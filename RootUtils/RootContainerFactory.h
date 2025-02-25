9c467fa6ff33ea8eb0ba8b5e2d45b37a64970ed2
Irene <irene.zoi@cern.ch>
2025-02-25 17:48:52 -0600

    use CBC instead of Chip as done for PS module

--- RootUtils/RootContainerFactory.h
+++ RootUtils/RootContainerFactory.h
@@ -162,6 +162,7 @@ void bookHistogramsFromStructure(TFile*                   theOutputFile,
                 for(const auto chip: *hybrid)
                 {
                     std::string chipFolderType = "Chip";
+                    if((chip)->getFrontEndType() == FrontEndType::CBC3) chipFolderType = "CBC";
                     if((chip)->getFrontEndType() == FrontEndType::MPA2) chipFolderType = "MPA";
                     if((chip)->getFrontEndType() == FrontEndType::SSA2) chipFolderType = "SSA";
                     std::string chipFolder     = "/" + chipFolderType + "_" + std::to_string(chip->getId());
