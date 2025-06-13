49156e6272c92bf77000bd9713a63a41d742010a
Irene Zoi <irene.zoi@cern.ch>
2025-06-13 13:56:29 -0500

    getting the pedestal from the pedestal calibration

--- tools/OTinjectionDelayOptimization.cc
+++ tools/OTinjectionDelayOptimization.cc
@@ -150,7 +150,8 @@ void OTinjectionDelayOptimization::optimizeInjectionDelay()
                                 if(theChipFrontEndType == FrontEndType::CBC3) distanceFromThreshold = -expectedNoise * fCBCnumberOfSigmaNoiseAwayFromPedestal;
                                 if(theChipFrontEndType == FrontEndType::SSA2) distanceFromThreshold = expectedNoise * fSSAnumberOfSigmaNoiseAwayFromPedestal;
                                 if(theChipFrontEndType == FrontEndType::MPA2) distanceFromThreshold = expectedNoise * fMPAnumberOfSigmaNoiseAwayFromPedestal;
-                                float thePedestal                  = float(theChipBestThresholdAndDelay.first) / numberOfIterations;
+                                // float thePedestal                  = float(theChipBestThresholdAndDelay.first) / numberOfIterations;
+                                float thePedestal                  = theChip->getAveragePedestal();
                                 float theBestThreshold             = thePedestal + distanceFromThreshold;
                                 theChipBestThresholdAndDelay.first = std::round(theBestThreshold);
                             }
