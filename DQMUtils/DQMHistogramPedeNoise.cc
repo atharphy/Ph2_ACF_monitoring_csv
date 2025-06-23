5ea9c242a657024400870f3028c91fa301aa027b
Irene Zoi <irene.zoi@cern.ch>
2025-06-23 10:49:17 -0500

    fix root bug after operating system automatic update

--- DQMUtils/DQMHistogramPedeNoise.cc
+++ DQMUtils/DQMHistogramPedeNoise.cc
@@ -984,6 +984,7 @@ void DQMHistogramPedeNoise::fitSCurves()
                             if(cOpticalGroup->getFrontEndType() == FrontEndType::OuterTracker2S)
                             {
                                 TF1* cFit = new TF1("SCurveFit", MyErf, cChannelPedestal - (cChannelNoise * 5), cChannelPedestal + (cChannelNoise * 5), 2);
+                                cFit->SetNpx(100);
                                 cFit->SetParameter(0, cChannelPedestal);
                                 cFit->SetParameter(1, cChannelNoise);
 
