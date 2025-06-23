6aed6d5c7fc3b9eea75cd2b2c4de000c7e9909d3
Irene Zoi <irene.zoi@cern.ch>
2025-06-23 10:51:59 -0500

    forgot PS module part

--- DQMUtils/DQMHistogramPedeNoise.cc
+++ DQMUtils/DQMHistogramPedeNoise.cc
@@ -1001,6 +1001,7 @@ void DQMHistogramPedeNoise::fitSCurves()
                             else if((cOpticalGroup->getFrontEndType() == FrontEndType::OuterTrackerPS))
                             {
                                 TF1* cFit = new TF1("SCurveFit", MyErfc, cChannelPedestal - (cChannelNoise * 5), cChannelPedestal + (cChannelNoise * 5), 2);
+                                cFit->SetNpx(100);
                                 cFit->SetParameter(0, cChannelPedestal);
                                 cFit->SetParameter(1, cChannelNoise);
 
