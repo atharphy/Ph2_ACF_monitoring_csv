883b415137037ad87d4134c69043522649cac969
Irene Zoi <irene.zoi@cern.ch>
2025-11-24 13:31:28 -0600

    updated scale factor for SSA occupancy

--- HWDescription/Definition.h
+++ HWDescription/Definition.h
@@ -171,6 +171,6 @@ enum class EventType
 
 #define L1_UNSPARSFIED_BLOCK_SIZE_2S 11
 
-#define SSA_NOISE_SIGMA_SCALE_FACTOR 0.7 // based on the measurement at FNAL
+#define SSA_NOISE_SIGMA_SCALE_FACTOR 0.75 // based on the measurement at FNAL
 
 #endif
