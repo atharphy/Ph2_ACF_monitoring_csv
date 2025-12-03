9625efb88fefd852c669c1e4088600bd35603e4e
Fabio Ravera <fabio.ravera@cern.ch>
2025-12-03 14:22:18 -0600

    Fixed bug in OTverifyBoardDataWord

--- tools/OTverifyBoardDataWord.cc
+++ tools/OTverifyBoardDataWord.cc
@@ -412,6 +412,11 @@ void OTverifyBoardDataWord::prepareFWForL1IntegrityTest(Ph2_HwDescription::BeBoa
     cVecReg.push_back({"fc7_daq_cnfg.tlu_block.tlu_enabled", 0x0});
     cVecReg.push_back({"fc7_daq_cnfg.readout_block.global.data_handshake_enable", 0x1});
     fBeBoardInterface->WriteBoardMultReg(theBoard, cVecReg);
+
+    for(auto theOpticalGroup: *theBoard)
+    {
+        for(auto theHybrid: *theOpticalGroup) { fCicInterface->SetSparsification(static_cast<OuterTrackerHybrid*>(theHybrid)->fCic, true); }
+    }
 }
 
 uint8_t OTverifyBoardDataWord::getNumberOfBytesInSinglePacket(OpticalGroup* cOpticalGroup) const
