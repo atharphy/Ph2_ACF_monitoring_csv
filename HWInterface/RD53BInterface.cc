39f086e2f4af2861ad9eb6c254c8ae1127151a79
dinardo <mauro.dinardo@cern.ch>
2025-12-10 15:24:38 +0100

    Minor

--- HWInterface/RD53BInterface.cc
+++ HWInterface/RD53BInterface.cc
@@ -979,7 +979,7 @@ float RD53BInterface::measureTemperature(ReadoutChip* pChip, uint32_t data, cons
         uint16_t       maxADC       = RD53Shared::setBits(pRD53RegMap.at("DAC_NTC").fBitSize);
         uint16_t       midADC       = (minADC + maxADC) / 2;
         const uint16_t numberOfBits = floor(log2(maxADC - minADC + 1) + 1);
-        const uint16_t maxVal       = maxADC;
+        const uint16_t maxVal       = RD53Shared::setBits(pRD53RegMap.at("MonitorConfig").fBitSize - 1);
         uint16_t       maxADCval    = 0;
         uint16_t       it           = 0;
         uint16_t       nSteps       = pChip->getRegItem("SAMPLE_NTC_SLOPE").fValue;
@@ -1009,15 +1009,14 @@ float RD53BInterface::measureTemperature(ReadoutChip* pChip, uint32_t data, cons
         // #########################################################################
         // # Scan from 0 to saturation to compute ADC volt independent temperature #
         // #########################################################################
-        const uint16_t       step    = maxADCval / nSteps;
-        const uint16_t       regSize = RD53Shared::setBits(pRD53RegMap.at("MonitorConfig").fBitSize - 1);
+        const uint16_t       step = maxADCval / nSteps;
         std::vector<int32_t> ntcVoltVec;
         std::vector<int32_t> ntcCurrVec;
         std::vector<int32_t> ntcADCVec;
         for(uint16_t i = 1; i < nSteps; i++)
         {
             RD53BInterface::readNTCvoltCurr(pChip, step * i, ntcVolt, ntcCurr);
-            if((ntcVolt > 0) && (ntcVolt < regSize) && (ntcCurr > 0) && (ntcCurr < regSize))
+            if((ntcVolt > 0) && (ntcVolt < maxVal) && (ntcCurr > 0) && (ntcCurr < maxVal))
             {
                 ntcVoltVec.push_back(ntcVolt);
                 ntcCurrVec.push_back(ntcCurr);
