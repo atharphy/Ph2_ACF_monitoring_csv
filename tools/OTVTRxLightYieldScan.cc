9e647baac421b61a5e209f4842043dd1a46af237
Irene <irene.zoi@cern.ch>
2026-02-03 09:54:31 -0600

    formatAll

--- tools/OTVTRxLightYieldScan.cc
+++ tools/OTVTRxLightYieldScan.cc
@@ -90,12 +90,11 @@ void OTVTRxLightYieldScan::scanVTRxLightYield()
                 D19cFWInterface* theFWinterface = dynamic_cast<D19cFWInterface*>(fBeBoardInterface->getFirmwareInterface(theBoard));
                 for(auto theOpticalGroup: *theBoard)
                 {
-                    bool registersWritten = false;
+                    bool     registersWritten       = false;
                     uint16_t numberOfAttempts       = 0;
                     uint16_t maximumAllowedAttempts = 10;
                     while(!registersWritten)
                     {
-
                         registersWritten = fVTRxInterface->WriteChipMultReg(theOpticalGroup->fVTRx, theRegisterValues, true);
                         if(++numberOfAttempts >= maximumAllowedAttempts) break;
                     }
@@ -106,10 +105,10 @@ void OTVTRxLightYieldScan::scanVTRxLightYield()
                         VTRxLightYieldRX = -1;
                         continue;
                     }
-                    else  
+                    else
                         VTRxLightYieldRX = theFWinterface->GetSFPParameter(theOpticalGroup, "RX");
-                    
-                        theOpticalPowerContainer.getOpticalGroup(theBoard->getId(), theOpticalGroup->getId())->getSummary<float>() = VTRxLightYieldRX;
+
+                    theOpticalPowerContainer.getOpticalGroup(theBoard->getId(), theOpticalGroup->getId())->getSummary<float>() = VTRxLightYieldRX;
                     if(fOfStream != nullptr)
                     {
                         j["data"]["VTRxLightYield"][std::to_string(theBoard->getId())][std::to_string(theOpticalGroup->getId())][std::to_string(biasValue)][std::to_string(modulationValue)] =
