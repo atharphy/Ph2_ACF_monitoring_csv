e6d2c034d711d45b5f4a2ca5bf82fa472be25bb0
PSI CMS Service Account <psi.cms@cern.ch>
2026-02-16 08:37:11 +0100

    revised control, adding temperature sensors including differential poly resistor measurements

--- ProductionToolsIT/RD53BMuxReader.cc
+++ ProductionToolsIT/RD53BMuxReader.cc
@@ -44,10 +44,50 @@ void RD53BMuxReader::Stop()
     RD53RunProgress::reset();
 }
 
+//float RD53BMuxReader::get_adc(
+RD53BMuxReader::adc_result RD53BMuxReader::get_adc(
+			      Ph2_HwInterface::RD53Interface* chipInterface,
+			      Ph2_HwDescription::ReadoutChip* chip,
+			      const std::string & name,
+			      size_t nsample
+			      )
+{
+  unsigned int sum_adc = 0;
+  std::stringstream line;
+  unsigned int n =0;
+  std::vector<float> adc_values;
+  for(unsigned int n = 0; n < nsample; n++){
+    const auto value = chipInterface->ReadChipADC(chip, name);
+    if (value <  4096){
+      sum_adc += value;
+      line << std::fixed << std::setw(5) << value;
+      adc_values.push_back(value);
+    }else{
+      line << RED << std::fixed << std::setw(5) << value << YELLOW;
+    }
+  }
+  // basic outlier rejection
+  float sum=0, rms=0;
+  unsigned int n_valid = 0;
+  for(const auto v : adc_values){
+    if (abs(v-adc_mean) < 10){
+      sum += v;
+      var += pow(v-adc_mean, 2);
+      n_valid++;
+    }
+  }
+  
+  if (n_valid > 0){
+    return {float(sum)/n_valid, sqrt(var)/n_valid, line.str()};
+  }else{
+    return {0,0,""};
+  }
+}
+
 void RD53BMuxReader::configure(const std::string & args){
   // see HWInterface/RD53BInterface.cc
     std::vector<std::string> all = {
-      "Iref", "NTC_VOLT",
+      "Iref", "NTC_VOLT", "NTC_CURR",
       "ANA_IN_CURR", "ANA_SHUNT_CURR", "DIG_IN_CURR", "DIG_SHUNT_CURR",
       "VIND", "VINA","VDDD", "VDDA","VOFS","VrefA","VrefD","Vref_CORE", "Vref_PRE",
       "POLY_TEMPSENS_TOP", "POLY_TEMPSENS_BOTTOM",
@@ -64,6 +104,7 @@ void RD53BMuxReader::configure(const std::string & args){
 
     muxlist.clear();
     use_wlt_calibration = true;
+    read_temperatures = false;    // override by adding 'temperatures' to the adc list
     
     if (args == ""){
       for(size_t i=0; i< default_adc.size(); i++){
@@ -85,21 +126,24 @@ void RD53BMuxReader::configure(const std::string & args){
 	 }
        }else if (arg=="no_wlt_calibration"){
 	 use_wlt_calibration = false;
-       }else{
-	 // assume it's an ADC name
-	 if (arg.find("*") == std::string::npos){
-	   // standard match
+       }else if (arg=="temperatures"){
+	 read_temperatures = true;
+       }else if (std::find(all.begin(), all.end(), arg) != all.end()){
 	   muxlist.push_back(arg);
-	 }else{
-	   //wildcard
-	   const auto pattern = std::regex_replace(arg, std::regex(R"(\*)"), ".*");
-	   regex re("^" + pattern + "$");
-	   for(size_t i=0; i< all.size(); i++){
-	     const auto  adc = all[i];
-	     if (std::regex_match(adc, re)){
-		 muxlist.push_back(adc);
-	     }
-	   }	 
+       }else{
+	 //possible wildcard
+	 bool found_match = false;
+	 const auto pattern = std::regex_replace(arg, std::regex(R"(\*)"), ".*");
+	 regex re("^" + pattern + "$");
+	 for(size_t i=0; i< all.size(); i++){
+	   const auto  adc = all[i];
+	   if (std::regex_match(adc, re)){
+	     muxlist.push_back(adc);
+	     found_match = true;
+	   }
+	 }
+	 if (!found_match){
+	   LOG(WARNING) << RED << "[RD53BMuxReader::configure]  unknown adc " << arg << RESET;
 	 }
        }
     }
@@ -110,10 +154,19 @@ void RD53BMuxReader::localConfigure(const std::string& histoFileName, int curren
     LOG(INFO) << GREEN << "[RD53BMuxReader::localConfigure] Starting run: " << BOLDYELLOW << theCurrentRun << RESET;
 }
 
+
+
+
 void RD53BMuxReader::run()
 {
     auto chipInterface = static_cast<RD53Interface*>(this->fReadoutChipInterface);
 
+    std::vector<std::string>  tempsens_list = {
+      "TEMPSENS_ANA_SLDO", "TEMPSENS_CENTER", "TEMPSENS_DIG_SLDO",
+      "RADSENS_ANA_SLDO", "RADSENS_DIG_SLDO", "RADSENS_CENTER",
+      "POLY_TEMPSENS_TOP", "POLY_TEMPSENS_BOTTOM", "INTERNAL_NTC_ABS"
+    };
+    
     CalibBase::prepareChipQueryForEnDis("chipSubset"); //  ??
     const float              R_IMUX  = 4.99;  // kOhm, R17(ABCD) on TEPX hdis
     const float              V_REF   = 0.845; // TEPX  84.5 k x Iref x 2.5, nominal according to RD53B manual
@@ -138,54 +191,52 @@ void RD53BMuxReader::run()
 		    float slope  = V_REF / 4096;
 		    
 		    if (use_wlt_calibration){
-		      offset = cChip->getRegItem("ADC_OFFSET_VOLT").fValue * 1e-4; // [V] sic  
+		      offset = cChip->getRegItem("ADC_OFFSET_VOLT").fValue * 1e-4; // [V]
 		      slope = (cChip->getRegItem("ADC_MAXIMUM_VOLT").fValue * 1e-3 -  offset) / 4096.; // [V/ADC];
-		      if(use_wlt_calibration){
+		      if(use_wlt_calibration and (muxlist.size()>0)){
 			LOG(INFO) << "applying wlt calibration    slope = " << std::setw(8) << std::setprecision(3) << slope*1e3 << " (mV/ADC),   offset =" << offset*1e3 << " (mV)";
 		      }
-		    }else{
-		      // "calibrate" the current measurement offset, assuming the ntc dac has no offset
-		      float        x_sum = 0, y_sum = 0, x2_sum = 0, xy_sum = 0;
-		      unsigned int n = 0;
-		      for(unsigned int idac = 10; idac < 100; idac += 10)
-			{
+
+		    // esitmate the adc offset assuming the ntc current dac has no offset (i.e. dac =0 means current = 0)
+		    float x_sum = 0, y_sum = 0, x2_sum = 0, xy_sum = 0;
+		    unsigned int n = 0;
+		    for(unsigned int idac = 0; idac < 50; idac += 10)
+		      {
                         chipInterface->WriteChipReg(cChip, "DAC_NTC", idac);
                         const auto adc = chipInterface->ReadChipADC(cChip, "NTC_CURR");
                         if((adc > 0) && (adc < 4096))
 			  {
-                            n += 1;
+			    n += 1;
                             x_sum += idac;
                             y_sum += adc;
                             x2_sum += idac * idac;
                             xy_sum += adc * idac;
 			  }
-			}
-		      float legacy_offset = (x2_sum * y_sum - x_sum * xy_sum) / (x2_sum * n - x_sum * x_sum);
-		      offset = -legacy_offset / slope; 
-		      chipInterface->WriteChipReg(cChip, "DAC_NTC", 100); // back to the default value
-		      if(offset > 50)
-			{
-			  LOG(INFO) << "ignoring offset " << offset;
-			  offset = 0;
-			}
-		    }
+		      }
+		    float test_offset_adc = (x2_sum * y_sum - x_sum * xy_sum) / (x2_sum * n - x_sum * x_sum);
+		    LOG(INFO) << "adc offset measurement = " << std::setw(10) << std::setprecision(2) << test_offset_adc <<  " (ADC)"
+			      << "           <->           " << -test_offset_adc * slope * 1e3 << " (mV)";
+		      
+		    // "standard" value for the current dac, affects NTC and POLY_SENS
+		    chipInterface->WriteChipReg(cChip, "DAC_NTC", 100);
 		    
                     // raw ADC: for a list of "observables" see RD53BInterface::getADCobservable  in HWInterface/RD53BInterface.cc
                     const auto sampleNtimes                    = cChip->getRegItem("SAMPLE_N_TIMES").fValue;
                     cChip->getRegItem("SAMPLE_N_TIMES").fValue = 1; // local averaging
+		    
                     for(const auto& mux: muxlist)
                     {
+			/*
                         uint32_t           adc_sum       = 0;
                         const unsigned int n_measurement = 12; // TODO configurable
                         unsigned int       n_valid       = 0;
                         std::stringstream  line;
-
                         // get the mean value of n_measurement tries
                         std::vector<float> adc_values;
                         for(unsigned int n = 0; n < n_measurement; n++)
                         {
                             const auto value = chipInterface->ReadChipADC(cChip, mux);
-                            if((value > 0) && (value < 4096))
+                            if(value < 4096)
                             {
                                 line << std::fixed << std::setw(5) << value;
                                 adc_values.push_back(value);
@@ -222,8 +273,10 @@ void RD53BMuxReader::run()
                                 }
                             }
                         }
-
-                        //float value = adc_mean * V_REF / 4096; // nominal 12 bit ADC
+			*/
+			  
+		        const auto result = read_adc(chipInterface, cChip, mux, 12);
+			float value = result.value * slope + offset;
 			float value = adc_mean * slope + offset;
                         std::string unit = "V ";
                         if(mux == "Iref")
@@ -249,7 +302,7 @@ void RD53BMuxReader::run()
 			  value *= 4;
 			  unit  = "V ";
                         }
-                        else if((mux == "VDDA") || (mux == "VDDD"))
+                        else if( (mux == "VDDA") || (mux == "VDDD"))
                         {
 			  value *= 2;
 			  unit  = "V ";
@@ -260,13 +313,77 @@ void RD53BMuxReader::run()
 			  unit  = "A "; 
                         }
 			
-                        LOG(INFO) << BOLDBLUE << std::setw(24) << mux << " ADC = " << BOLDYELLOW << line.str() << "  |   " << std::setw(7) << std::setprecision(1) << adc_mean << "   " << std::setw(9)
+			  //LOG(INFO) << BOLDBLUE << std::setw(24) << mux << " ADC = " << BOLDYELLOW << line.str() << "  |   " << std::setw(7) << std::setprecision(1) << adc_mean << "   " << std::setw(9)
+                          //        << std::setprecision(3) << value << " " << unit << RESET;
+                        LOG(INFO) << BOLDBLUE << std::setw(24) << mux << " ADC = " << BOLDYELLOW << result.lline << "  |   " << std::setw(7) << std::setprecision(1) << result.value << "   " << std::setw(9)
                                   << std::setprecision(3) << value << " " << unit << RESET;
                         summary[mux].push_back(make_pair(value, unit));
+
                     } // mux list
                     chip_currents.push_back(Icroc);
                     cChip->getRegItem("SAMPLE_N_TIMES").fValue = sampleNtimes; // restore the original vaue
 
+		    if (read_temperatures){
+		      {
+			// read poly sensor ntc style (simplified, temporary)
+			float const R_ref = 4.99; // TEPX HDI
+			float const R_poly_27C = 11.07; //10.8;    //  kOhm : nominal, should come from WLT
+			float const TC_poly = 0.22e-2;     //  0.22 %/C  
+			// R(T) = R(27 C) * (1+ TC * (T-27))
+			// R(T)/R(27C) = 1  +  TC * (T-27)
+			// T = 27 + (R(T)/R(27C) -1 )/ TC
+			const unsigned int dac_1 = 100;
+			const unsigned int dac_2 = 400;
+
+			chipInterface->WriteChipReg(cChip, "DAC_NTC", dac_1);
+			usleep(1000000);
+			const auto ref_adc_1  = get_adc(chipInterface, cChip, "NTC_CURR", 5).value;
+			const auto poly_top_1 = get_adc(chipInterface, cChip, "POLY_TEMPSENS_TOP", 5).value;
+			const auto rgnd_top_1 = get_adc(chipInterface, cChip, "ANA_GND_1", 5).value;// 0x14 =  20      VGNDA_RPOLYTSENS_TOP
+			const auto poly_bot_1 = get_adc(chipInterface, cChip, "POLY_TEMPSENS_BOTTOM", 5).value;
+			const auto rgnd_bot_1 = get_adc(chipInterface, cChip, "ANA_GND_0", 5).value; // 0x13 = 19 VGNDA_RPOLYTSENS_BOTTOM 
+			
+			chipInterface->WriteChipReg(cChip, "DAC_NTC", dac_2);
+			usleep(1000000);
+			const auto ref_adc_2  = get_adc(chipInterface, cChip, "NTC_CURR", 5).value;
+			const auto poly_top_2 = get_adc(chipInterface, cChip, "POLY_TEMPSENS_TOP", 5).value;
+			const auto rgnd_top_2 = get_adc(chipInterface, cChip, "ANA_GND_1", 5).value;// 0x14 =  20      VGNDA_RPOLYTSENS_TOP
+			const auto poly_bot_2 = get_adc(chipInterface, cChip, "POLY_TEMPSENS_BOTTOM", 5).value;
+			const auto rgnd_bot_2 = get_adc(chipInterface, cChip, "ANA_GND_0", 5).value; // (0x13 = 19 VGNDA_RPOLYTSENS_BOTTOM )
+
+			const float R_poly_top = float(poly_top_2 - poly_top_1 - rgnd_top_2 + rgnd_top_1) / float(ref_adc_2 - ref_adc_1) * R_ref;
+			const float R_poly_bot = float(poly_bot_2 - poly_bot_1 - rgnd_bot_2 + rgnd_bot_1) / float(ref_adc_2 - ref_adc_1) * R_ref;
+			const float T_poly_top = 27.0 + (R_poly_top / R_poly_27C - 1.0) / TC_poly;
+			const float T_poly_bot = 27.0 + (R_poly_bot / R_poly_27C - 1.0) / TC_poly;
+
+			LOG(INFO) << "ref    " << ref_adc_2  << " " << ref_adc_1  << RESET;
+			LOG(INFO) << "top    " << poly_top_2 << " - " << rgnd_top_2 << "    " << poly_top_1  << " - " << rgnd_top_1 << "   " << R_poly_top << "   " << T_poly_top << " C" << RESET;
+			LOG(INFO) << "bottom " << poly_bot_2  << " - " << rgnd_bot_2 << "    " << poly_bot_1   << " - " << rgnd_bot_1  << "   " << R_poly_bot << "   " << T_poly_bot << " C" << RESET;
+			
+			summary["POLY_TEMPSENS_TOP_T"].push_back(std::make_pair(T_poly_top, "C "));
+			summary["POLY_TEMPSENS_BOTTOM_T"].push_back(std::make_pair(T_poly_bot, "C "));
+
+			LOG(INFO) << BOLDBLUE << std::setw(24) << "POLY_TEMPSENS_TOP_R"
+				  << "   R = " << BOLDYELLOW << std::setw(9) << std::setprecision(3) << R_poly_top << RESET;
+			LOG(INFO) << BOLDBLUE << std::setw(24) << "POLY_TEMPSENS_BOTTOM_R"
+				  << "   R = " << BOLDYELLOW << std::setw(9) << std::setprecision(3) << R_poly_bot << RESET;
+			LOG(INFO) << BOLDBLUE << std::setw(24) << "POLY_TEMPSENS_TOP_T"
+				  << "   T = " << BOLDYELLOW << std::setw(7) << std::setprecision(1) << T_poly_top << RESET;
+			LOG(INFO) << BOLDBLUE << std::setw(24) << "POLY_TEMPSENS_BOTTOM_T"
+				  << "   T = " << BOLDYELLOW << std::setw(7) << std::setprecision(1) << T_poly_bot << RESET;
+			
+			chipInterface->WriteChipReg(cChip, "DAC_NTC", 100);
+		      }
+
+		      
+		      // all other temperature sensors
+		      for(const auto& tempsens: tempsens_list){
+			if ((tempsens == "POLY_TEMPSENS_TOP") || (tempsens == "POLY_TEMPSENS_BOTTOM")) continue;
+			float value = chipInterface->ReadChipMonitor(cChip, tempsens, true);
+			summary[tempsens+"_T"].push_back(std::make_pair(value, "C "));
+			LOG(INFO) << BOLDBLUE << std::setw(24) << tempsens+"_T" << "   T = " << BOLDYELLOW << std::setw(7) << std::setprecision(1) << value << RESET;
+		      }
+		    }
                 } // chips
 
                 std::stringstream header, current;
@@ -277,16 +394,26 @@ void RD53BMuxReader::run()
                     current << std::setw(9) << std::setprecision(3) << chip_currents[c] << " A ";
                     module_current += chip_currents[c];
                 }
-                LOG(INFO) << BOLDBLUE << std::setw(20) << "chip id"
+                LOG(INFO) << BOLDBLUE << std::setw(24) << "chip id"
                           << "  | " << header.str() << RESET;
-                LOG(INFO) << BOLDBLUE << std::setw(20) << "chip current"
-                          << "  | " << current.str() << "  sum = " << std::setw(9) << std::setprecision(3) << module_current << " A" << RESET;
+		if (module_current > 0){
+		  LOG(INFO) << BOLDBLUE << std::setw(24) << "chip current"
+			    << "  | " << current.str() << "  sum = " << std::setw(9) << std::setprecision(3) << module_current << " A" << RESET;
+		}
                 for(const auto& mux: muxlist)
                 {
                     std::stringstream line;
                     for(const auto& result: summary[mux]) { line << std::setw(9) << std::setprecision(3) << result.first << " " << std::setw(2) << result.second; }
-                    LOG(INFO) << BOLDBLUE << std::setw(20) << mux << "  | " << line.str() << RESET;
+                    LOG(INFO) << BOLDBLUE << std::setw(24) << mux << "  | " << line.str() << RESET;
                 }
+		if (read_temperatures){
+		  for(const auto& tempsens: tempsens_list)
+		    {
+		      std::stringstream line;
+		      for(const auto& result: summary[tempsens + "_T"]) { line << std::setw(9) << std::setprecision(3) << result.first << " " << std::setw(2) << result.second; }
+                    LOG(INFO) << BOLDBLUE << std::setw(24) << tempsens+"_T" << "  | " << line.str() << RESET;
+		    }
+		}
             }
 
     // ##################
--- ProductionToolsIT/RD53BMuxReader.h
+++ ProductionToolsIT/RD53BMuxReader.h
@@ -11,6 +11,8 @@
 #define RD53BMuxReader_H
 
 #include "../tools/RD53CalibBase.h"
+#include "../HWInterface/RD53Interface.h"
+#include "HWDescription/RD53.h"
 
 #ifdef __USE_ROOT__
 #include "DQMUtils/RD53VoltageTuningHistograms.h"
@@ -29,6 +31,7 @@ class RD53BMuxReader : public CalibBase
         //   delete histos;
     }
 
+  
     void Running() override;
     void Stop() override;
     void ConfigureCalibration() override;
@@ -41,13 +44,21 @@ class RD53BMuxReader : public CalibBase
     void analyze();
     void configure(const std::string & args);
 
+    struct adc_result{
+      float value;
+      float error;
+      std::string line;
+    };
+  
+    adc_result get_adc(Ph2_HwInterface::RD53Interface* chipInterface, Ph2_HwDescription::ReadoutChip* chip, const std::string & name, unsigned int nsample);
+  
   private:
     void fillHisto() override;
-
   protected:
     int theCurrentRun;
     std::vector<std::string> muxlist;
     bool use_wlt_calibration;
+    bool read_temperatures;
 };
 
 #endif
