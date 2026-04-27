ab13bf14f10479cc8c0d575fec80348d95840b3a
dinardo <mauro.dinardo@cern.ch>
2026-04-27 23:20:36 +0200

    Minor

--- tools/RD53PowerTrimming.h
+++ tools/RD53PowerTrimming.h
@@ -60,7 +60,7 @@ class PowerTrimming : public CalibBase
     uint16_t   MAX_PREAMP;
     uint16_t   MAX_COMP;
     uint16_t   MAX_LDAC;
-    const bool doDebug{true};
+    const bool doDebug{false};
 
     std::vector<PowerTrimmingData> fPowerTrimmingResults;
 
